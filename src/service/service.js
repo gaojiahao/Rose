44b9f1cd7a1ef894929972758c30cb8e8539642c
baixia123 <xia.bai@refordom.com>
2018-01-09 09:37:17 +0800

    service增加接口请求

--- src/service/service.js
+++ src/service/service.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 export function getLogin(){
 	return new Promise((resolve, reject)=>{
-		//请求地址
 		axios.get("/H_roleplay-si/wxLogin?code=8_EZ-6GQqnNNiTFylKBtC1O6fc0y0Ppp_MHi3x1s2k8&state=1")
 		.then((response)=>{
 			console.log(response);
