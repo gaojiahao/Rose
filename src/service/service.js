7dc6dc1425d8ffc938a8ffcd79088d3ad9d10681
baixia123 <xia.bai@refordom.com>
2018-01-09 13:55:27 +0800

    增加任务请求

--- src/service/service.js
+++ src/service/service.js
@@ -1,9 +1,24 @@
 import axios from 'axios'
 export function getLogin(){
 	return new Promise((resolve, reject)=>{
-		axios.get("/H_roleplay-si/wxLogin?code=8_EZ-6GQqnNNiTFylKBtC1O6fc0y0Ppp_MHi3x1s2k8&state=1")
+		axios.get("/H_roleplay-si/wxLogin?code=Cz5XDOLO6BROKnUepF-qSr7d86wY-ujA4TQ2vNW494s&state=1")
 		.then((response)=>{
-			console.log(response);
+			console.log(response.data);
+			const result = response.data;
+			resolve(result);
+		})
+		
+		
+	})
+}
+export function getTask(){
+	return new Promise((resolve, reject)=>{
+		axios.get("/H_roleplay-si/ds/getMyToDoTask", {headers:{
+			"authorization":"a6fd04a8db6e46fca3a10a2d98cffd65MOBILE"
+			}
+		})
+		.then((response)=>{
+			console.log(response.data);
 			const result = response.data;
 			resolve(result);
 		})
