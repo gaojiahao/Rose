5c44a6595fcf20aec0819b71622c99c8f546b053
baixia123 <xia.bai@refordom.com>
2018-01-09 19:02:42 +0800

    增加已办，详情接口请求

--- src/service/service.js
+++ src/service/service.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
-export function getLogin(){
+/*
+export function getLogin(code){
 	return new Promise((resolve, reject)=>{
-		axios.get("/H_roleplay-si/wxLogin?code=Cz5XDOLO6BROKnUepF-qSr7d86wY-ujA4TQ2vNW494s&state=1")
+		axios.get("/H_roleplay-si/wxLogin?code="+code+"&state=1")
 		.then((response)=>{
-			console.log(response.data);
 			const result = response.data;
 			resolve(result);
 		})
@@ -11,15 +11,71 @@ export function getLogin(){
 		
 	})
 }
-export function getTask(){
+export function getTask(token){
+	return new Promise((resolve, reject)=>{
+		axios.get("/H_roleplay-si/ds/getMyToDoTask", {headers:{
+			"authorization":token
+			}
+		})
+		.then((response)=>{
+			const result = response.data;
+			resolve(result);
+		})
+		
+		
+	})
+}
+ */
+//测试代码
+export function getLogin(code){
+	return new Promise((resolve, reject)=>{
+		axios.get("/H_roleplay-si/wxLogin?code=Cz5XDOLO6BROKnUepF-qSr7d86wY-ujA4TQ2vNW494s&state=1")
+		.then((response)=>{
+			const result = response.data;
+			resolve(result);
+		})
+		
+		
+	})
+}
+export function getTask(token){
 	return new Promise((resolve, reject)=>{
 		axios.get("/H_roleplay-si/ds/getMyToDoTask", {headers:{
 			"authorization":"a6fd04a8db6e46fca3a10a2d98cffd65MOBILE"
 			}
 		})
 		.then((response)=>{
-			console.log(response.data);
 			const result = response.data;
+			resolve(result);
+		})
+		
+		
+	})
+}
+export function getDoneTask(token){
+	return new Promise((resolve, reject)=>{
+		axios.get("/H_roleplay-si/ds/getMyDoneTask", {headers:{
+			"authorization":"a6fd04a8db6e46fca3a10a2d98cffd65MOBILE"
+			}
+		})
+		.then((response)=>{
+			const result = response.data;
+			resolve(result);
+		})
+		
+		
+	})
+}
+export function getListTask(transCode){
+	return new Promise((resolve, reject)=>{
+		axios.get("/H_roleplay-si/ds/listTaskLogByTransCode",{headers:{
+			"authorization":"a6fd04a8db6e46fca3a10a2d98cffd65MOBILE"
+			},params:{
+				transCode:transCode
+			}
+		})
+		.then((response)=>{
+			const result = response.data.tableContent;
 			resolve(result);
 		})
 		
