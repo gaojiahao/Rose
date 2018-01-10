e8ba77ed13ceca22def4360e8ab399c2bbf908e3
baixia123 <xia.bai@refordom.com>
2018-01-10 11:09:25 +0800

    token过期重新获取

--- src/service/service.js
+++ src/service/service.js
@@ -1,5 +1,4 @@
 import axios from 'axios'
-/*
 export function getLogin(code){
 	return new Promise((resolve, reject)=>{
 		axios.get("/H_roleplay-si/wxLogin?code="+code+"&state=1")
@@ -25,37 +24,10 @@ export function getTask(token){
 		
 	})
 }
- */
-//测试代码
-export function getLogin(code){
-	return new Promise((resolve, reject)=>{
-		axios.get("/H_roleplay-si/wxLogin?code=Cz5XDOLO6BROKnUepF-qSr7d86wY-ujA4TQ2vNW494s&state=1")
-		.then((response)=>{
-			const result = response.data;
-			resolve(result);
-		})
-		
-		
-	})
-}
-export function getTask(token){
-	return new Promise((resolve, reject)=>{
-		axios.get("/H_roleplay-si/ds/getMyToDoTask", {headers:{
-			"authorization":"a6fd04a8db6e46fca3a10a2d98cffd65MOBILE"
-			}
-		})
-		.then((response)=>{
-			const result = response.data;
-			resolve(result);
-		})
-		
-		
-	})
-}
 export function getDoneTask(token){
 	return new Promise((resolve, reject)=>{
 		axios.get("/H_roleplay-si/ds/getMyDoneTask", {headers:{
-			"authorization":"a6fd04a8db6e46fca3a10a2d98cffd65MOBILE"
+			"authorization":token
 			}
 		})
 		.then((response)=>{
@@ -66,10 +38,10 @@ export function getDoneTask(token){
 		
 	})
 }
-export function getListTask(transCode){
+export function getListTask(transCode,token){
 	return new Promise((resolve, reject)=>{
 		axios.get("/H_roleplay-si/ds/listTaskLogByTransCode",{headers:{
-			"authorization":"a6fd04a8db6e46fca3a10a2d98cffd65MOBILE"
+			"authorization":token
 			},params:{
 				transCode:transCode
 			}
@@ -82,3 +54,4 @@ export function getListTask(transCode){
 		
 	})
 }
+
