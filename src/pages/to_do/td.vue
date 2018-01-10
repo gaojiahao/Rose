d4958d9da7a2d511d4d3bc6630fe097b976a6a73
刘治增 <zionstark.liu@refordom.com>
2018-01-10 16:34:09 +0800

    新增判断用户进入的方式

--- src/pages/to_do/td.vue
+++ src/pages/to_do/td.vue
@@ -67,7 +67,7 @@
 		mounted(){ 		
 			iscroll = new IScroll('#td', {
 				probeType: 2,
-				tap:true
+				click:true
 			});
 			iscroll.on('scrollStart', ()=>{			
 				if(iscroll.maxScrollY < 0){
@@ -83,14 +83,22 @@
 		},
 		created(){
 			var url = location.href;
-			//var url = "https://rfd.roletask.com/Rose/?code=3hrrgxM6-91PRPrpbSoZDqwgR_KCXxImZ5dcRiMhlME&state=1#/to_do";
-			var arr = url.split("?");
-			var arr1 = arr[1].split("&")
-			var code = arr1[0].split("=")[1];
-			console.log(code);
-			getLogin(code).then((result)=>{
-				localStorage.setItem("token",result.token);
-			})
+			// 判断是否是从推送消息进来
+			if(url.indexOf('code')>0){
+				var arr = url.split("?");
+				var arr1 = arr[1].split("&")
+				var code = arr1[0].split("=")[1];
+				console.log(code);
+				getLogin(code).then((result)=>{
+					localStorage.setItem("token",result.token);
+				})
+			}else{
+				// 用户主动点击进入页面
+				var rr = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=ww3c1aa17c16e380b7&redirect_uri=https%3A%2F%2Frfd.roletask.com%2FRose&response_type=code&scope=snsapi_base&agentid=1000017&state=1#wechat_redirect'
+				window.location.href = rr;
+			}
+			
+			
 			var token = localStorage.getItem("token");
 			console.log(token);
 			if(token){
