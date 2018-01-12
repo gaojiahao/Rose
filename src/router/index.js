4913534c96d7a38058cc2b770f529a788cea1425
baixia123 <xia.bai@refordom.com>
2018-01-12 15:19:56 +0800

    修改路由地址

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import To_do from '../pages/to_do/td.vue'
 import Finish from '../pages/finish/finish.vue'
 import Td_detail from "../pages/to_do/td_detail.vue"
+import Finish_detail from "../pages/finish/finish_detail.vue"
 import VueWechatTitle from 'vue-wechat-title';  
 import Welcome from "../pages/welcome/welcome.vue"
 Vue.use(VueWechatTitle)
@@ -21,13 +22,29 @@ export default new Router({
     	component:To_do ,
     	meta: {title: '我的待办'},
     	children:[
-	    	{ path:'detail',
+	    	{ path:'do_detail',
 	    	 component:Td_detail
 	    	}
-    	 ]
-    	},
-		{ path:'/finish', component:Finish ,meta: {title: '我的已办'}},
-		{ path:'/welcome', component:Welcome },
-    { path:'/', redirect:'/welcome'}
+    	]
+    },
+	{ 
+		path:'/finish',
+		component:Finish ,
+		meta: {title: '我的已办'},
+		children:[
+			{ path:'done_detail',
+				component:Finish_detail
+			}
+		]
+	},
+	{ 
+		path:'/welcome', 
+		component:Welcome		 
+	},
+    { 
+		path:'/',
+		redirect:'/welcome'
+	}
+		
   ]
 })
