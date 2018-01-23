e1b1233ed6f6fe7de7c6ee4d779db3d628de21a2
baixia123 <xia.bai@refordom.com>
2018-01-23 13:51:23 +0800

    注释vux

--- src/pages/to_do/td.vue
+++ src/pages/to_do/td.vue
@@ -45,8 +45,6 @@
 
 <script>
 	import {getTask} from '../../service/service.js'
-	import { Group, Cell } from 'vux'
-
     export default{
     	data(){
     		return{
@@ -55,55 +53,20 @@
 				list:[]
     		}
 		},
-		components: {
-			Group,
-			Cell
-		},
     	methods:{
     		goDetail(tab){
 					this.$router.push({path:"/to_do/do_detail",query:{
 						info:tab
 					}})
 			}
-		// 	upCallback(page) {
-		// 		var self = this;
-		// 		getListDataFromNet(page.num, page.size, function(curPageData) {
-		// 			if(page.num == 1) self.listArr = [];
-		// 			self.listArr = self.listArr.concat(curPageData);
-		// 			self.mescroll.endSuccess(curPageData.length);
-		// 			console.log(self.listArr);
-				
-		// 		}, function() {
-		// 			//联网失败的回调,隐藏下拉刷新和上拉加载的状态;
-		// 			self.mescroll.endErr();
-		// 		});
-		// 		function getListDataFromNet(pageNum,pageSize,successCallback,errorCallback){
-		// 			setTimeout(function () {
-		// 				var data=self.list;
-		// 				var listData=[];//模拟分页数据
-		// 				for (var i = (pageNum-1)*pageSize; i < pageNum*pageSize; i++) {
-		// 					if(i==data.length) break;
-		// 					listData.push(data[i]);
-		// 				}
-		// 				successCallback&&successCallback(listData);//成功回调
-						
-		// 			},500)
-		// 		}
-				
-				
-		// 	}
-    	},
+	  	},
 		mounted(){ 	
 			var self = this;
 			self.mescroll = new MeScroll("td",{
 				up:{
-					// callback:self.upCallback,
 					isBounce:false,
 					use:false
-					// page:{
-					// 	num:0,
-					// 	size:10
-					// }											
+															
 				},
 				down:{
 					use:false
@@ -120,10 +83,13 @@
 				
 			})
 			
+		
+		}	
 			
-		}
 			
-    }
+	}
+			
+   
 </script>
 
 <style>
