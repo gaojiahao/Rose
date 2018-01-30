a6854fa71e311c064d03a19aa3c025f1c868c140
baixia123 <xia.bai@refordom.com>
2018-01-30 14:34:37 +0800

    修改流程图样式

--- src/pages/to_do/td_detail.vue
+++ src/pages/to_do/td_detail.vue
@@ -1,6 +1,6 @@
 <template>
-<transition enter-active-class="slideInRight"
-		leave-active-class="slideOutRight">
+<!-- <transition enter-active-class="slideInRight"
+		leave-active-class="slideOutRight"> -->
 		
     <div id="td_detail" >
 		<div id="mescroll" class='mescroll'>
@@ -99,14 +99,14 @@
 					<cell title="需求名称" :value="detailInfo.requirementProject.requireName" value-align="left"></cell>
 					<cell title="提交人" :value="detailInfo.baseinfo.creatorName" value-align="left" ></cell>
 					<cell title="创建时间" :value="detailInfo.baseinfo.crtTime" value-align="left" ></cell>
-					<cell title="应用类型" :value="detailInfo.requirementProject.appType" value-align="left" v-if="!detailInfo.requirementProject.appType.value"></cell>
+					<cell title="应用类型" :value="detailInfo.requirementProject.appType" value-align="left" v-if="!detailInfo.requirementProject.appType.selection"></cell>
 					<cell title="应用类型" :value="detailInfo.requirementProject.appType.value" value-align="left" v-if="detailInfo.requirementProject.appType.value"></cell>
 					<cell title="应用名称" :value="detailInfo.requirementProject.appName" value-align="left" v-if="!detailInfo.requirementProject.appName.value"></cell>
 					<cell title="应用名称" :value="detailInfo.requirementProject.appName.value" value-align="left" v-if="detailInfo.requirementProject.appName.value"></cell>
-					<cell title="实现方式" :value="detailInfo.requirementProject.provideType" value-align="left" v-if="!detailInfo.requirementProject.provideType.value"></cell>
+					<cell title="实现方式" :value="detailInfo.requirementProject.provideType" value-align="left" v-if="!detailInfo.requirementProject.provideType.selection"></cell>
 					<cell title="实现方式" :value="detailInfo.requirementProject.provideType.value" value-align="left" v-if="detailInfo.requirementProject.provideType.value"></cell>
 					<cell title="优先级" :value="detailInfo.requirementProject.level.value" value-align="left" v-if="detailInfo.requirementProject.level.value"></cell>
-					<cell title="优先级" :value="detailInfo.requirementProject.level" value-align="left" v-if="!detailInfo.requirementProject.level.value"></cell>
+					<cell title="优先级" :value="detailInfo.requirementProject.level" value-align="left" v-if="!detailInfo.requirementProject.level.selection"></cell>
 					<datetime  format="YYYY-MM-DD HH:mm" @on-change="change" title="预计交付时间" placeholder="请选择" value-align="left" v-if="!detailInfo.requirementProject.etc"></datetime>				
 					<cell 
 						title="用户故事"
@@ -122,13 +122,6 @@
 						:arrow-direction="show1 ? 'up' : 'down'"
 						@click.native="show1 = !show1"></cell>
 					<cell-box v-if="show1">{{detailInfo.requirementProject.acceptStandard}}</cell-box>
-					<!-- <cell 
-						title="需求评审意见"
-						is-link
-						:border-intent="false"
-						:arrow-direction="choiceShow ? 'up' : 'down'"
-						@click.native="choiceShow = !choiceShow"></cell>
-					<cell-box v-if="choiceShow">{{detailInfo.requirementProject.approvalOpinion}}</cell-box> -->
 					<cell 
 						title="工作流"
 						is-link
@@ -204,31 +197,176 @@
 						@click.native="show = !show"></cell>
 
 				</group>
+				<!-- ZNLH -->
+				<group label-width="4.5em" label-margin-right="2em" label-align="left" v-if="(detailInfo.transCode).indexOf('ZNLH')>=0">
+					<cell title="需求编码" :value="detailInfo.transCode" value-align="left" ></cell>
+					<cell title="需求名称" :value="detailInfo.baseinfo.transType2" value-align="left" ></cell>
+					<cell title="提交人" :value="detailInfo.baseinfo.creatorName" value-align="left" ></cell>
+					<cell title="创建时间" :value="detailInfo.baseinfo.crtTime" value-align="left" ></cell>
+					<cell title="职能" :value="detailInfo.baseinfoExt.varchar1.value" value-align="left" ></cell>
+					<cell title="职能经理" :value="detailInfo.baseinfoExt.varchar2.value" value-align="left" ></cell>
+					<cell title="周次" :value="detailInfo.baseinfoExt.varchar3.value" value-align="left" ></cell>
+					<cell title="例会时间" :value="detailInfo.baseinfoExt.varchar4" value-align="left" ></cell>
+					<cell 
+						title="本周关键数据"
+						is-link
+						:border-intent="false"
+						:arrow-direction="datashow ? 'up' : 'down'"
+						@click.native="datashow = !datashow" width="7em"></cell>
+					<cell-box v-if="datashow">{{detailInfo.baseinfoExt.varchar5}}</cell-box>
+					<cell 
+						title="本周改善结果"
+						is-link
+						:border-intent="false"
+						:arrow-direction="resultshow ? 'up' : 'down'"
+						@click.native="resultshow = !resultshow"></cell>
+					<cell-box v-if="resultshow">{{detailInfo.baseinfoExt.varchar6}}</cell-box>
+					<cell 
+						title="改善机会与下周战略"
+						is-link
+						:border-intent="false"
+						:arrow-direction="nextshow ? 'up' : 'down'"
+						@click.native="nextshow = !nextshow"></cell>
+					<cell-box v-if="nextshow">{{detailInfo.baseinfoExt.varchar7}}</cell-box>
+					<cell 
+						title="解决方案"
+						is-link
+						:border-intent="false"
+						:arrow-direction="resolvetshow ? 'up' : 'down'"
+						@click.native="resolvetshow = !resolvetshow"></cell>
+					<cell-box v-if="resolvetshow">{{detailInfo.baseinfoExt.varchar8}}</cell-box>
+					<cell 
+						title="工作流"
+						is-link
+						:border-intent="false"
+						:arrow-direction="show ? 'up' : 'down'"
+						@click.native="show = !show"></cell>
+
+				</group>
+				<!-- YHFK -->
+				<group label-width="4.5em" label-margin-right="2em" label-align="left" v-if="(detailInfo.transCode).indexOf('YHFK')>=0">
+					<cell title="需求编码" :value="detailInfo.transCode" value-align="left" ></cell>
+					<cell title="需求名称" :value="detailInfo.baseinfo.transType2" value-align="left" ></cell>
+					<cell title="提交人" :value="detailInfo.baseinfo.creatorName" value-align="left" ></cell>
+					<cell title="创建时间" :value="detailInfo.baseinfo.crtTime" value-align="left" ></cell>
+					<cell title="客户" :value="detailInfo.transDetail.var1.value" value-align="left" ></cell>
+					<cell title="用户" :value="detailInfo.transDetail.var2" value-align="left" ></cell>
+					<cell title="反馈时间" :value="detailInfo.transDetail.date1" value-align="left" ></cell>
+					<cell title="反馈分类" :value="detailInfo.transDetail.var5.value" value-align="left" ></cell>
+					<cell title="紧急分类" :value="detailInfo.transDetail.var6.text" value-align="left" ></cell>
+					<cell title="期望解决时间" :value="detailInfo.transDetail.date2" value-align="left" ></cell>
+					<cell title="是否新问题" :value="detailInfo.transDetail.var7.text" value-align="left" ></cell>
+					<cell title="应用类型" :value="detailInfo.transDetail.var8.value" value-align="left" ></cell>
+					<cell title="应用名称" :value="detailInfo.transDetail.var9" value-align="left" ></cell>
+					<cell title="问题简述" :value="detailInfo.transDetail.var10" value-align="left" ></cell>
+					<cell 
+						title="工作流"
+						is-link
+						:border-intent="false"
+						:arrow-direction="show ? 'up' : 'down'"
+						@click.native="show = !show"></cell>
+
+				</group>
+				<!-- ZSK -->
+				<group label-width="4.5em" label-margin-right="2em" label-align="left" v-if="(detailInfo.transCode).indexOf('ZSK')>=0">
+					<cell title="需求编码" :value="detailInfo.transCode" value-align="left" ></cell>
+					<cell title="需求名称" :value="detailInfo.baseinfo.transType2" value-align="left" ></cell>
+					<cell title="提交人" :value="detailInfo.baseinfo.creatorName" value-align="left" ></cell>
+					<cell title="创建时间" :value="detailInfo.baseinfo.crtTime" value-align="left" ></cell>
+					<cell title="标题" :value="detailInfo.requirement.requireName" value-align="left" ></cell>
+					<cell title="知识标签" :value="detailInfo.requirement.requireNature.value" value-align="left" ></cell>
+					<cell 
+						title="如何调坑里"
+						is-link
+						:border-intent="false"
+						:arrow-direction="failshow ? 'up' : 'down'"
+						@click.native="failshow = !failshow" width="7em"></cell>
+					<cell-box v-if="failshow">{{detailInfo.requirement.userStory}}</cell-box>
+					<cell 
+						title="得到的共识与知识"
+						is-link
+						:border-intent="false"
+						:arrow-direction="acceptshow ? 'up' : 'down'"
+						@click.native="acceptshow = !acceptshow" width="7em"></cell>
+					<cell-box v-if="acceptshow">{{detailInfo.requirement.acceptStandard}}</cell-box>
+					<cell 
+						title="工作流"
+						is-link
+						:border-intent="false"
+						:arrow-direction="show ? 'up' : 'down'"
+						@click.native="show = !show"></cell>
+
+				</group>
+				<!-- QKXQ -->
+				<group label-width="4.5em" label-margin-right="2em" label-align="left" v-if="(detailInfo.transCode).indexOf('QKXQ')>=0">
+					<cell title="需求编码" :value="detailInfo.transCode" value-align="left" ></cell>
+					<cell title="项目名称" :value="detailInfo.requirementJob.projectName.value" value-align="left" ></cell>
+					<cell title="提交人" :value="detailInfo.baseinfo.creatorName" value-align="left" ></cell>
+					<cell title="创建时间" :value="detailInfo.baseinfo.crtTime" value-align="left" ></cell>
+					<cell title="需求名称" :value="detailInfo.requirementJob.requireName" value-align="left" ></cell>
+					<cell title="需求类型" :value="detailInfo.requirementJob.requireNature.value" value-align="left" ></cell>
+					<cell title="应用类型" :value="detailInfo.requirementJob.appType.value" value-align="left" ></cell>
+					<cell title="应用名称" :value="detailInfo.requirementJob.appName" value-align="left" ></cell>
+					<cell title="实现方式" :value="detailInfo.requirementJob.provideType.value" value-align="left" ></cell>
+					<cell 
+						title="用户故事"
+						is-link
+						:border-intent="false"
+						:arrow-direction="storyshow ? 'up' : 'down'"
+						@click.native="storyshow = !storyshow"></cell>
+					<cell-box v-if="storyshow">{{detailInfo.requirementJob.userStory}}</cell-box>
+					<cell 
+						title="验收标准"
+						is-link
+						:border-intent="false"
+						:arrow-direction="show1 ? 'up' : 'down'"
+						@click.native="show1 = !show1"></cell>
+					<cell-box v-if="show1">{{detailInfo.requirementJob.acceptStandard}}</cell-box>
+					<cell 
+						title="工作流"
+						is-link
+						:border-intent="false"
+						:arrow-direction="show ? 'up' : 'down'"
+						@click.native="show = !show"></cell>
+
+				</group>
 				<div class='process limit' v-if="show">
-					
 						<div class='agree_status'>
 							<div v-for="(tab,index) in infoList" class='allInfo' :key="index">
+								<flow orientation="vertical">
+									<flow-state :is-done="tab.status?true:false" :state="index+1"></flow-state>
+									<flow-line  :is-done="tab.status?true:false" ></flow-line>
+									
+								</flow>
 								<div class='info' >
-									<i class='iconfont icon-shenfenzheng'></i>
 									<p>
 										<span>{{tab.userName}}</span>
-										<span>{{tab.nodeName}}</span>
-										
+										<span >{{tab.nodeName}}</span>	
 									</p>
 									<p>
-										<span>{{tab.endTime}}</span>
+										<span v-if="tab.endTime">{{tab.endTime}}</span>
 									</p>
-									<p v-if="tab.message"><span>审批意见：{{tab.message}}</span></p>
-									<i class=" weui-icon weui-icon-success" v-if="tab.status"></i>
-									<i class=" weui-icon weui-icon-waiting" v-if="!tab.status" style="top:15px;"></i>
-
-									
 								</div>
-								<div class='iconfont icon-xia arrow' ></div>
 							</div>
+						</div>
+						<div class="choice">
+							<ul>
+								<li v-for="(tab,index) in infoList" :key="index" v-if="tab.message">
+									<p class='title'>审批意见</p>
+									<i class='iconfont icon-yonghu'></i>
+									<div class="choice_content">
+										<p>
+											<span>{{tab.userName}}</span>
+											<em>{{tab.endTime}}</em>
+										</p>
+										<p>{{tab.message}}</p>
+									</div>
+								</li>
+							</ul>
 						</div>
 									
 				</div>
+				
 			</div>
 		</div>
 		<div class="btn">
@@ -247,12 +385,12 @@
 		</div>				      
 	</div>
 	
-</transition>
+<!-- </transition> -->
 </template>
 
 <script>
 import { getListTask,getDetailInfo} from '../../service/service.js'
-import { Group, Cell,Datetime,CellBox,Confirm,XInput ,XTextarea ,TransferDomDirective as TransferDom } from 'vux'
+import { Group, Cell,Datetime,CellBox,Confirm,XInput ,XTextarea ,Flow, FlowState, FlowLine ,TransferDomDirective as TransferDom } from 'vux'
 export default{
 	data(){
 		return{
@@ -266,6 +404,12 @@ export default{
 			bugshow:false,
 			confirmshow:false,
 			show2:false,
+			datashow:true,
+			nextshow:false,
+			resultshow:false,
+			resolvetshow:false,
+			acceptshow:false,
+			failshow:false,
 			list:[]
 		}
 	},
@@ -279,9 +423,10 @@ export default{
 		CellBox,
 		Confirm,
 		XInput ,
-		XTextarea 
-		
-		
+		XTextarea ,
+		Flow, 
+		FlowState,
+		 FlowLine 
 	},
 	methods:{
 		agree(){
@@ -310,9 +455,9 @@ export default{
 		let token  = localStorage.getItem("token");
 		let code = this.$route.query.data;
 		let time = new Date().getTime();
-		console.log(token);
 		getListTask(code,token).then((result)=>{
 			this.infoList = result;
+			console.log(result);
 			
 		})
 		getDetailInfo(time,token,code).then((result)=>{
@@ -320,8 +465,8 @@ export default{
 				this.detailInfo = JSON.parse(result[0].json_data);
 			}
 			var t = JSON.parse(result[0].json_data);
+			console.log(t);
 		})
- 
 
 	},
 	mounted(){	
@@ -378,7 +523,20 @@ text-align: left !important;
 .weui-dialog__bd:first-child {
 	padding:0px !important; 
 }
-
+.weui-wepay-flow, .weui-wepay-flow-auto{
+	padding: 0 !important;
+	position: absolute;
+	left:-20px;
+	top:15px;
+	height:100%;
+}
+.weui-wepay-flow__li .weui-wepay-flow__state {
+    width: 20px !important;
+    height: 20px !important;
+	border-radius: 50% !important;;
+	line-height: 20px !important;;
+    
+}
 .btn{
 	position: fixed;
 	left: 0;
@@ -403,53 +561,41 @@ text-align: left !important;
 }
 .process{
 	width:100%;
-	background: #FFF;
-	
+	background: #F0F2F5;	
 }
 .process .agree_status{	
-	margin:15px;
+	background: #FFF;
+	padding-bottom:10px;
+	
+}
+.agree_status .allInfo{
+	margin-left:30px;
+	position: relative;
 }
 .agree_status .info{
-	border:1px solid #ccc;
-	padding: 15px 10px;
+	border-bottom:1px solid #ccc;
+	padding: 10px;
 	position: relative;
 	vertical-align: middle;
-}
-.allInfo:first-child{
-	margin-top:10px;
 }
 .allInfo:last-child{
 	margin-bottom:10px;
 }
-.info .iconfont{
-	position: absolute;
-	top:20px;
-}
 .allInfo:last-child .arrow{
+	display: none;
+}
+#td_detail .allInfo:last-child .weui-wepay-flow__line{
 	display: none;
 }
 .info p{
 	display: block;
 	margin-left:30px;
+	font-size:15px;
 }
 .info p span{
 	display: inline-block;
 	margin-bottom:5px;
 	margin-right: 20px;
-}
-.process_over{
-	border: 1px solid #ccc;
-    padding: 10px;
-	text-align: center;
-	color:#E64340;
-}
-.weui-icon{
-	position:absolute;
-	right:15px;
-	top:25px;
-}
-.arrow{
-	text-align: center;
 }
 .qjDetail{
 	padding: 15px;
@@ -482,4 +628,51 @@ text-align: left !important;
 .qgProcess:last-child .arrow{
 	display: none;
 }
+.choice{
+	width:100%;
+	margin-top:45px;
+	background: #fff;
+}
+.choice P{
+	font-size:15px;
+}
+.choice ul li{
+	border-bottom: 1px solid #d9d9d9;
+	position: relative;
+}
+.choice ul li p.title{
+	position:absolute;
+	left:0;
+	top:-32px;
+	width:100%;
+	border-bottom: 1px solid #d9d9d9;
+	font-size:15px;
+	padding-left: 20px;
+	background:#fff;
+	display:none;
+	margin-bottom:0;
+	height:32px;
+	line-height:32px;
+}
+.choice ul li:first-child .title{
+	display:block;
+}
+.choice ul li .iconfont{
+	position: absolute;
+	left:20px;
+	top:15px;
+}
+ul li .choice_content{
+	padding-left: 50px;
+}
+.choice .choice_content>p{
+	width:100%;
+	line-height: 30px;
+	padding-right:10px;
+}
+
+.choice_content p em{
+	float:right;
+}
+
 </style>
