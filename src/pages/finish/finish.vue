ccffb3504919b9fa072605dc8b98daaf05daaf6a
baixia123 <xia.bai@refordom.com>
2018-01-31 17:21:44 +0800

    增加分页功能

--- src/pages/finish/finish.vue
+++ src/pages/finish/finish.vue
@@ -1,21 +1,16 @@
 <template>
 	<div style="height:100%;">
-		<div id="finish" class="page mescroll" >
-			<div class='wrapper'>
-				<div class="weui-search-bar" id="searchBar">
-					<form class="weui-search-bar__form">
-						<div class="weui-search-bar__box">
-							<i class="weui-icon-search"></i>
-							<input type="search" class="weui-search-bar__input" id="searchInput" placeholder="搜索" required="">
-							<a href="javascript:" class="weui-icon-clear" id="searchClear"></a>
-						</div>
-						<label class="weui-search-bar__label" id="searchText">
-							<i class="weui-icon-search"></i>
-							<span>搜索</span>
-						</label>
-					</form>
-					<a href="javascript:" class="weui-search-bar__cancel-btn" id="searchCancel">取消</a>
-				</div>
+		<div id="finish" class="page" >
+			<search
+			@result-click="resultClick"
+			@on-change="getResult"
+			position="absolute"
+			v-model="value"
+			@on-focus="onFocus"
+			@on-cancel="onCancel"
+			@on-submit="onSubmit"
+			ref="search"></search>
+			<div class='mescroll' id="fmescroll" style="padding-bottom:55px;">
 				<ul class='list' id='finishTask' >
 					<li @click="goDetail(tab.businessKey)" v-for="(tab,index) in doneList" :key="index">
 						<p>
@@ -39,82 +34,106 @@
 						</p>        		
 					</li>
 				</ul>
-			</div>   
+			</div>	  
     	</div>
-    	
-		<router-view></router-view>
-    	
-		
+		<router-view></router-view>	
 	</div>
 </template>
 
 <script>
 import { getLogin,getDoneTask } from '../../service/service.js'
+import { Search } from 'vux'
     export default{
     	data(){
     		return{				
 				doneList:[],
 				mescroll:null,
 				show:true,
-				list:[]
+				list:[],
+				value:""
 				
     		}
     	},
+		components: {
+			Search
+		},
     	methods:{
     		goDetail(param){
 				this.$router.push({path:"/Rose/finish/done_detail",query:{
 					data:param
 				}})
-			}
-		// 	upCallback(page) {
-		// 		var self = this;
-		// 		getListDataFromNet(page.num, page.size, function(curPageData) {
-		// 			if(page.num == 1) self.doneList = [];
-		// 			self.doneList = self.doneList.concat(curPageData);
-		// 			self.mescroll.endSuccess(curPageData.length);
-		// 			console.log(self.doneList);
+			},
+			setFocus () {
+				this.$refs.search.setFocus()
+			},
+			resultClick (item) {
+				window.alert('you click the result item: ' + JSON.stringify(item))
+			},
+			getResult (val) {
+			// console.log('on-change', val)
+			// this.results = val ? getResult(this.value) : []
+			},
+			onSubmit (value) {
+				// this.$refs.search.setBlur()
+				// this.$vux.toast.show({
+				// 	type: 'text',
+				// 	position: 'top',
+				// 	text: 'on submit'
+				// })
+				console.log(value);
+			},
+			onFocus () {
+				console.log('on focus')
+			},
+			onCancel () {
+				console.log('on cancel')
+			},
+			upCallback(page) {
+				var self = this;
+				getListDataFromNet(page.num, function(curPageData) {
+					self.doneList = self.doneList.concat(curPageData);
+					self.mescroll.endSuccess(curPageData.length);
+					console.log(self.doneList);
 				
-		// 		}, function() {
-		// 			//联网失败的回调,隐藏下拉刷新和上拉加载的状态;
-		// 			self.mescroll.endErr();
-		// 		});
-		// 		function getListDataFromNet(pageNum,pageSize,successCallback,errorCallback){
-		// 			setTimeout(function () {
-		// 				var data=self.list;
-		// 				var listData=[];//模拟分页数据
-		// 				for (var i = (pageNum-1)*pageSize; i < pageNum*pageSize; i++) {
-		// 					if(i==data.length) break;
-		// 					listData.push(data[i]);
-		// 				}
-		// 				successCallback&&successCallback(listData);//成功回调
+				}, function() {
+					//联网失败的回调,隐藏下拉刷新和上拉加载的状态;
+					self.mescroll.endErr();
+				});
+				function getListDataFromNet(pageNum,successCallback,errorCallback){
+					setTimeout(function () {
+						var token = localStorage.getItem("token");
+						getDoneTask(token,pageNum).then((res)=>{
+							successCallback&&successCallback(res.tableContent);//成功回调
+						})
 						
-		// 			},500)
-		// 		}
+						
+					},500)
+				}
 				
 				
-		// 	}
+			}
+			
+
     	
 		},
 		created(){
 			var token = localStorage.getItem("token");
-				getDoneTask(token).then((res)=>{
-					//this.list = res.tableContent;
-					this.doneList = res.tableContent;
-					console.log(res);
-				})
+			var page =1;
+			getDoneTask(token,page).then((res)=>{
+				this.doneList = res.tableContent;
+			})
 			
 		},
 		mounted(){						
 			var self = this;
-			self.mescroll = new MeScroll("finish",{
+			self.mescroll = new MeScroll("fmescroll",{
 				up:{
 					isBounce:false,
-					use:false
-					// page:{
-					// 	num:0,
-					// 	size:10
-					// },
-					// callback: self.upCallback
+					page:{
+						num:1,
+						size:10
+					},
+					callback: self.upCallback
 				},
 				down:{
 					use:false
