25c3c6f307e64c72b69103b8a929709c77b30063
unknown <sheng.lai@refordom.com>
2018-04-17 20:30:49 +0800

    tablast

--- src/components/my_record.vue
+++ src/components/my_record.vue
@@ -16,7 +16,7 @@
         <form-preview 
             class="project_part"
             header-label="金额" 
-            header-value="¥330000.00" 
+            :header-value="'￥'+list1Total" 
             :body-items="list1">
         </form-preview>
 
@@ -24,14 +24,14 @@
         <form-preview 
             class="project_part"
             header-label="金额" 
-            header-value="¥230000.00" 
+            :header-value="'￥'+list2Total" 
             :body-items="list2">
         </form-preview>
         <divider>B类产品</divider>
         <form-preview 
             class="project_part"
             header-label="金额" 
-            header-value="¥230000.00" 
+            :header-value="'￥'+list3Total" 
             :body-items="list3">
         </form-preview>
 
@@ -59,12 +59,15 @@ export default {
             list1:[],
             list2:[],
             list3:[],
+            list1Total:0,
+            list2Total:0,
+            list3Total:0,
             list4:[
                     { label: '足金999',value: '20000 件/套' }, 
                     { label: '千金888',value: '12312 件/套' }, 
                     { label: '千足金666',value: '4232 件/套' }
                 ],
-            remark:''
+            remark:'',
         }
     },
     methods:{
@@ -87,21 +90,27 @@ export default {
             this.list2.length=0;
             this.list3.length=0;
             this.list4.length=0;
+            this.list1Total=0;
+            this.list2Total=0;
+            this.list3Total=0;
             for(let i = 0 ;i<m.length; i++){
                 if(m[i].objType==''){
+                    this.list1Total += Number(m[i].amount);
                     this.list1.push({
-                        label:m[i].creator,
-                        value:m[i].amount,
+                        label:'数量',
+                        value:m[i].qty,
                     })
                 }else if(m[i].objType=='A'){
+                    this.list2Total += Number(m[i].amount);
                         this.list2.push({
-                        label:m[i].creator,
-                        value:m[i].amount,
+                        label:'数量',
+                        value:'',
                     })
                 }else if(m[i].objType=='B'){
+                    this.list3Total += Number(m[i].amount);
                         this.list3.push({
-                        label:m[i].creator,
-                        value:m[i].amount,
+                        label:'数量',
+                        value:'',
                     })
                 }
             }
