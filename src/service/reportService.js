bc19cc1e0fc567fb7d5111a6bff52e868af8f433
xuehongjie <jie.xue@refordom.com>
2018-04-19 15:29:22 +0800

    '修改查看列表接口传值'

--- src/service/reportService.js
+++ src/service/reportService.js
@@ -10,7 +10,7 @@ let reportService = {
         dsCode: 'getStatementsList',
         shengName: data.shengName || '', // 区域
         bankName: data.bankName || '', // 银行
-        bmName: data.bmName || '', // 部门
+        sybName: data.bmName || '', // 部门
         objName: data.objName || '', // 项目
         // pageSize: data.pageSize || 30,
         // startRow: ((data.page || 1) - 1) * 30,
