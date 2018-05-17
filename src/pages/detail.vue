52d179340a47c331eaf473c781aee93e001e4ced
baixia123 <xia.bai@refordom.com>
2018-05-17 18:13:42 +0800

    详情页增加任务类型

--- src/pages/detail.vue
+++ src/pages/detail.vue
@@ -3,7 +3,9 @@
     <div id='mescroll' class="mescroll">
       <div>
           <!-- SSXQ -->
-         <ssxq :detailInfo='formInfo' v-if='code.indexOf("SSXQ")>=0'></ssxq>
+         <ssxq :detailInfo='formInfo' :status='taskStatus' v-if='code.indexOf("SSXQ")>=0'></ssxq>
+         <!-- CPXQ -->
+         <cpxq :detailInfo='formInfo' :status='taskStatus' v-if='code.indexOf("CPXQ")>=0'></cpxq>
        </div>
     </div>
   </div>
@@ -12,24 +14,40 @@
 <script>
   import getDetailService from './../service/getDetailService.js'
   import Ssxq from "./component/SSXQForm.vue"
+  import Cpxq from "./component/CPXQForm.vue"
+  import Mbgl from "./component/MBGLForm.vue"
   export default {
     data() {
       return {
-        formInfo:{},
-        code:''
+        formInfo : {},
+        code : '',
+        taskStatus : true
       }
     },
     components: {
-        Ssxq
+        Ssxq,
+        Cpxq,
+        Mbgl
     },
     methods: {
       
     },
     created() {
-        console.log(this.$route.query.code);
-        this.code = this.$route.query.code;
-        getDetailService.getDetailInfo(this.code).then(data=>{
-          console.log(data);
+        let code = this.$route.query.code,
+            status = this.$route.query.status;
+        this.code = code;
+        if(status == 'done'){
+          
+          this.taskStatus = false;
+        }
+        let formId = '';
+        if(code.indexOf('SSXQ') >= 0){
+          formId = 'a43aed46-8273-478a-9161-ef940542ed71'
+        }
+        else if(code.indexOf('CPXQ') >= 0){
+          formId = '1b282c62-fca0-4929-a581-253fe8f528bd'
+        }
+        getDetailService.getDetailInfo(formId,this.code).then( data=> {
           this.formInfo = data;
         })
 
@@ -67,14 +85,20 @@ text-align: left !important;
   box-sizing: border-box;
   width:100%;
   padding:10px;
+  color:#999;
+  font-size:17px !important;
+  line-height: 24px;
+  span{
+    font-size:17px !important;
+  }
+  
+   
 }
-// .vux-cell-box{
-//   color:#999;
-//   font-size:16px;
-//   line-height: 24px;
-// }
-// .vux-cell-box:not(:first-child):before{
-//     left:-10px !important;
-// }
+.weui-cells:after{
+  border-bottom: 0px solid #D9D9D9 !important;
+}
+.vux-cell-box:not(:first-child):before{
+    left:-10px !important;
+}
   
 </style>
