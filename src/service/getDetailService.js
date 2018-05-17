0cead6822253d50818e29b11502b2b4862a78fc0
baixia123 <xia.bai@refordom.com>
2018-05-17 18:29:54 +0800

    修改接口

--- src/service/getDetailService.js
+++ src/service/getDetailService.js
@@ -5,9 +5,9 @@ import conf from "../plugins/ajax/conf";
 let getDetailService={
 
     //获取任务详情
-    getDetailInfo(code) {
+    getDetailInfo(id,code) {
         return $axios.ajax({
-            url: '/H_roleplay-si/formAPI/findData/a43aed46-8273-478a-9161-ef940542ed71/'+code
+            url:`/H_roleplay-si/formAPI/findData/${id}/${code}`
             
         });
     },
