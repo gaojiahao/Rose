f7385c14aa911ed21be7711331866030a6d5d1c8
baixia123 <xia.bai@refordom.com>
2018-05-29 19:01:40 +0800

    修改分配给

--- src/pages/component/CSBUGForm.vue
+++ src/pages/component/CSBUGForm.vue
@@ -1,5 +1,5 @@
 <template>
-    <group label-width="6em" label-margin-right="2em" label-align="left" v-if='detailInfo.transCode_fgPlanInv'>
+    <group label-width="6.5em" label-margin-right="2em" label-align="left" v-if='detailInfo.transCode_fgPlanInv'>
         <cell 
             title="需求编码" 
             :value="detailInfo.transCode_fgPlanInv.values" 
@@ -7,7 +7,7 @@
         </cell>	
         <cell 
             title="BUG标题" 
-            :value="detailInfo.ssReqProjectName_fgPlanInv.values" 
+            :value="detailInfo.bugRequireName_fgPlanInv.values" 
             value-align="left">
         </cell>
         <cell 
@@ -22,43 +22,40 @@
         </cell>
         <cell 
             title="所属模块" 
-            :value="detailInfo.ssAppType_fgPlanInv.values" 
+            :value="detailInfo.bugRequireNature_fgPlanInv.values" 
             value-align="left">
         </cell>
          <cell 
             title="BUG类型" 
-            :value="detailInfo.ssAppType_fgPlanInv.values" 
+            :value="detailInfo.bugRequireDemension_fgPlanInv.values" 
             value-align="left">
         </cell>
         <cell 
             title="操作系统" 
-            :value="detailInfo.ssAppType_fgPlanInv.values" 
+            :value="detailInfo.operatingSystem_fgPlanInv.values" 
             value-align="left">
         </cell>
         <cell 
             title="浏览器" 
-            :value="detailInfo.ssAppType_fgPlanInv.values" 
-            value-align="left">
+            :value="detailInfo.browser_fgPlanInv.values" 
+            value-align="left"
+            v-if='detailInfo.browser_fgPlanInv.values.length>0'>
         </cell>
         <cell 
             title="迭代/项目" 
-            :value="detailInfo.ssAppType_fgPlanInv.values" 
+            :value="detailInfo.iterationProject_fgPlanInv.values" 
             value-align="left">
         </cell>
          <cell 
             title="严重程度" 
-            :value="detailInfo.ssAppType_fgPlanInv.values" 
+            :value="detailInfo.severity_fgPlanInv.values" 
             value-align="left">
         </cell>
-        <!-- <cell 
-            title="优先级" 
-            :value="detailInfo.requirementProject.level.value" 
-            value-align="left">
-        </cell> -->
-        <!-- <cell 
-        title="预计交付时间" 
-        :value="detailInfo.requirementProject.level.value" 
-        value-align="left"> -->
+        <cell
+            title="预计交付时间"
+            :value="formatTime(detailInfo.bugRequirementEtc_fgPlanInv.values)"
+            value-align="left"
+            v-if='!status&&detailInfo.requirementEtc_fgPlanInv.values'>
         </cell>
         <datetime  
             format="YYYY-MM-DD HH:mm" 
@@ -74,37 +71,109 @@
             :arrow-direction="bugShow ? 'up' : 'down'"
             @click.native="bugShow = !bugShow">
         </cell>
-        <cell-box v-if="bugShow">{{detailInfo.ssTechnicalAnalysis_fgPlanInv.values}}</cell-box>			
-        <group>
-            <cell 
-                title="用户故事"
-                is-link
-                :border-intent="false"
-                :arrow-direction="storyshow ? 'up' : 'down'"
-                @click.native="storyshow = !storyshow">
-            </cell>
-            <div  
-                v-html="detailInfo.ssUserStory_fgPlanInv.values"
-                class='htmlfiel vux-cell-box' 
-                v-if='storyshow'>
-            </div>  
-        </group>
-        <cell 
-            title="验收标准"
+         <div  
+            v-html="detailInfo.bugReproducStep_fgPlanInv.values"
+            class='htmlfiel vux-1px-t' 
+            v-if='bugShow'>
+        </div>  
+        <!-- <cell-box v-if="bugShow">{{detailInfo.bugReproducStep_fgPlanInv.values}}</cell-box>			 -->
+         <cell 
+            title="用户故事"
             is-link
             :border-intent="false"
-            :arrow-direction="acceptStandard ? 'up' : 'down'"
-            @click.native="acceptStandard = !acceptStandard">
+            :arrow-direction="storyshow ? 'up' : 'down'"
+            @click.native="storyshow = !storyshow"
+            v-if='detailInfo.bugUserStory_fgPlanInv.values.length>0'>
         </cell>
-        <cell-box v-if="acceptStandard">{{detailInfo.ssAcceptStandard_fgPlanInv.values}}</cell-box>
+        <div  
+            v-html="detailInfo.bugUserStory_fgPlanInv.values"
+            class='htmlfiel vux-1px-t' 
+            v-if='storyshow'>
+        </div> 
+        <group>
+            <div v-if='detailInfo.bugAcceptStandard_fgPlanInv.values.length>11'>
+                <cell 
+                    title="验收标准"
+                    is-link
+                    :border-intent="false"
+                    :arrow-direction="acceptStandard ? 'up' : 'down'"
+                    @click.native="acceptStandard = !acceptStandard">
+                </cell>
+                 <div  
+                    v-html="detailInfo.bugAcceptStandard_fgPlanInv.values"
+                    class='htmlfiel vux-1px-t' 
+                    v-if='acceptStandard'>
+                </div>  
+                <!-- <cell-box v-if="acceptStandard">{{detailInfo.cpAcceptStandard_fgPlanInv.values}}</cell-box> -->
+            </div>
+            <cell 
+                title="验收标准" 
+                :value="detailInfo.bugAcceptStandard_fgPlanInv.values" 
+                value-align="left"
+                v-else-if='detailInfo.bugAcceptStandard_fgPlanInv.values.length>0&&detailInfo.bugAcceptStandard_fgPlanInv.values.length<=11'>
+            </cell>
+        </group>
+        <group>
+            <div v-if='detailInfo.bugTechnicalAnalysis_fgPlanInv.values.length>11'>
+                <cell 
+                    title="需求技术分析"
+                    is-link
+                    :border-intent="false"
+                    :arrow-direction="technicalAnalysis ? 'up' : 'down'"
+                    @click.native="technicalAnalysis = !technicalAnalysis">
+                </cell>
+                <cell-box v-if="technicalAnalysis">{{detailInfo.bugTechnicalAnalysis_fgPlanInv.values}}</cell-box>
+            </div>
+            <cell 
+                title="需求技术分析" 
+                :value="detailInfo.bugTechnicalAnalysis_fgPlanInv.values" 
+                value-align="left"
+                v-else-if='detailInfo.bugTechnicalAnalysis_fgPlanInv.values.length>0&&detailInfo.cpTechnicalAnalysis_fgPlanInv.values.length<=11'>
+            </cell>
+        </group>
+        <group>
+            <div v-if='detailInfo.bugApprovalOpinion_fgPlanInv.values.length>11'>
+                 <cell 
+                    title="需求评审意见"
+                    is-link
+                    :border-intent="false"
+                    :arrow-direction="approvalOpinion ? 'up' : 'down'"
+                    @click.native="approvalOpinion = !approvalOpinion">
+                </cell>
+                <cell-box v-if="approvalOpinion">{{detailInfo.bugApprovalOpinion_fgPlanInv.values}}</cell-box>
+            </div>
+             <cell 
+                title="需求评审意见" 
+                :value="detailInfo.bugApprovalOpinion_fgPlanInv.values" 
+                value-align="left"
+                v-else-if='detailInfo.bugApprovalOpinion_fgPlanInv.values.length>0&&detailInfo.bugApprovalOpinion_fgPlanInv.values.length<=11'>
+            </cell>
+        </group>
+        <cell 
+            title="分配给" 
+            value-align="left" 
+            is-link 
+            @click.native="changeUser" 
+            :value='assignedName.nickname'
+            v-if='status&&!detailInfo.assignedTo_fgPlanInv.values'>
+        </cell>
 
     </group>
 </template>
 <script>
 import { Group, Cell , Datetime , CellBox} from 'vux'
+import {formatTime} from '../maps/date.js'
 export default {
     props:{
         'detailInfo':{
+            type : Object,
+            required : true
+        },
+        'status':{
+            type : Boolean,
+            required : true
+        },
+        'assignedName':{
             type : Object,
             required : true
         }
@@ -114,19 +183,28 @@ export default {
             acceptStandard:false,
             storyshow:false,
             bugShow:false,
-
+            approvalOpinion:false,
+            technicalAnalysis:false,
+            flowShow:false,
+            user:'',
+            distributionScroll: null
         }
     },
 	components: {
 		Group,
 		Cell,
 		Datetime,
-		CellBox,
+        CellBox
 	},
     methods:{
-        change(){
-
-        }
+        formatTime,
+        change(value){
+            this.$emit('date',value)
+        },
+        changeUser(){
+            //this.flowShow = !this.flowShow;
+            this.$emit("assigned",true)
+        },
     }
     
 }
