9a4109880cc7535e9ef99e75a4323098f2ebe590
sheng.lai@refordom.com <sheng.lai@reform.com>
2018-06-20 19:21:23 +0800

    时间戳格式化

--- src/components/sale_report.vue
+++ src/components/sale_report.vue
@@ -479,10 +479,7 @@ export default {
     timestampToTime(timestamp) {
       let date = new Date(timestamp), //时间戳为10位需*1000，时间戳为13位的话不需乘1000
         Y = date.getFullYear() + "-",
-        M =
-          (date.getMonth() + 1 < 10
-            ? "0" + (date.getMonth() + 1)
-            : date.getMonth() + 1) + "-",
+        M = (date.getMonth() + 1 < 10 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1) + "-",
         D = date.getDate() + " ";
       return Y + M + D;
     },
