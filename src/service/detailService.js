a018973059ca022d946db4d7388723cc36287985
刘治增 <zionstark.liu@refordom.com>
2018-07-25 21:36:11 +0800

    新增工作流接口

--- src/service/detailService.js
+++ src/service/detailService.js
@@ -8,14 +8,34 @@ let errorHandler = (e) => {
   });
   return Promise.reject(e)
 };
-export let getSOList = ( data = {})=>{
+// 工作流
+export let getWorkFlow = (data = {}) => {
     return $axios.ajax({
-        url:'/H_roleplay-si/formAPI/findData',
+        url: '/H_roleplay-si/ds/listTaskLogByTransCode',
+        data
+    }).catch( err => {
+        return errorHandler(err);
+    })
+}
+// 判断工作流节点是否与<我>有关
+export let isMyflow = (data = {}) => {
+    return $axios.ajax({
+        url: '/H_roleplay-si/ds/getWorkFlowInfoByTransCode',
+        data
+    }).catch( err => {
+        return errorHandler(err);
+    })
+}
+// 获取表单详情
+export let getSOList = (data = {}) => {
+    return $axios.ajax({
+        url: '/H_roleplay-si/formAPI/findData',
         data
     }).catch(e => {
         return errorHandler(e);
     });
 }
+
 export default{
     getSOList 
 }
