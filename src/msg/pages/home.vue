3577fa0205babbea43f61754ad22cfb91fe416d1
刘治增 <zionstark.liu@refordom.com>
2018-07-31 17:47:52 +0800

    消息页面新增样式

--- src/msg/pages/home.vue
+++ src/msg/pages/home.vue
@@ -1,12 +1,23 @@
 <template>
   <div class="inPage">
+    <!-- 搜索框 -->
+    <search></search>
+
     <div class="msg_list">
-      <div class="each_msg vux-1px-b">
-        <div class="app_img">
-          <img :src='defaultImg' alt="appImg">
-        </div>
+      <div class="each_msg">
         <div class="msg_info">
-          <div class="app_name">销售订单</div>
+          <!-- 图片 和 应用名称 -->
+          <div class="app_info">
+            <span class="app_img">
+              <img :src='defaultImg' alt="appImg">
+            </span>
+            <span class="app_name">销售订单</span>
+          </div>
+          <!-- 时间 -->
+          <div class="msg_time">15分钟前</div>
+        </div>
+        <div class="recv_msg">
+          您收到一条新的消息
         </div>
       </div>
     </div>
@@ -14,27 +25,70 @@
 </template>
 
 <script>
+import search from 'components/search'
 export default {
   data(){
     return{
       defaultImg: require('assets/sales-stock.png')
     }
+  },
+  components: {
+    search
   }
 }
 </script>
 
 <style lang='scss' scoped>
+.search {
+  margin: .1rem 0;
+}
 .each_msg {
-  display: flex;
+  width: 95%;
   padding: .1rem;
+  background: #fff;
+  margin: 0 auto .1rem;
   box-sizing: border-box;
-  .app_img {
-    width: .3rem;
-    height: .3rem;
-    img {
-      width: 100%;
-      border-radius: .06rem;
+  border-radius: .08rem;
+  box-shadow: 0 2px 10px #e8e8e8;
+  // 消息头部信息
+  .msg_info {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    span {
+      display: inline-block;      
     }
+    // 图片、应用名称
+    .app_info {
+      display: flex;
+      align-items: center;
+      // 图片
+      .app_img {
+        width: .24rem;
+        height: .24rem;
+        img {
+          width: 100%;
+          border-radius: .06rem;
+        }
+      }
+      // 应用名称
+      .app_name {
+        font-size: .14rem;
+        margin-left: .06rem;
+      }
+    }
+    // 产生时间
+    .msg_time {
+      font-size: .14rem;
+      color: #757575;
+    }
+  }
+  // 消息内容
+  .recv_msg {
+    color: #3a3a3a;
+    font-weight: 200;
+    margin-top: .1rem;
   }
 }
 </style>
