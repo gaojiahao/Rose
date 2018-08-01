5c23c18059ec3e529366dbd44e17674d6eb36709
刘治增 <zionstark.liu@refordom.com>
2018-08-01 15:07:11 +0800

    新增<应用消息列表>

---
+++ src/msg/pages/appMsgList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="pages">
+    <div class="list_top">
+      <search class="search"></search>
+    </div>
+    <div class="todo_msg">
+      <div class="msg_top">
+        
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import search from 'components/search'
+export default {
+  components: {
+    search
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+.list_top {
+  padding: .1rem 0;
+}
+
+
+</style>
