9801eb15063338e37c01dec518d2b3c2f5e97d79
刘治增 <zionstark.liu@refordom.com>
2018-08-06 10:22:06 +0800

    修改detail页面样式

--- src/components/detail/XSCKForm.vue
+++ src/components/detail/XSCKForm.vue
@@ -48,11 +48,11 @@
         <div class="title">物料列表</div>
         <div class="mater_list">
           <div class="each_mater vux-1px-b" v-for="(oItem, key) in orderList" :key='key'>
+            <div class="order_code">
+              <span class="order_title">所属订单</span>
+              <span class="order_num">{{key.replace(/_/g,'')}}</span>
+            </div>
             <div class="each_mater_wrapper">
-              <div class="order_code">
-                <span class="order_title">所属订单</span>
-                <span class="order_num">{{key.replace(/_/g,'')}}</span>
-              </div>
               <!-- 物料信息 -->
               <div class="order_matter" v-for="(item, index) in oItem" :key="index">
                 <div class="mater_img">
