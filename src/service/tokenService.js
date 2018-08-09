2a44a1e41744f28f6a5f3841d2b885c59f834a0a
sheng.lai@refordom.com <sheng.lai@reform.com>
2018-08-09 19:34:33 +0800

    市场宣传修改

--- src/service/tokenService.js
+++ src/service/tokenService.js
@@ -94,8 +94,8 @@ let tokenService = {
           },
           data: {
             loginModel: 1,
-            password: 'stark',
-            userCode: 'rfd9527'
+            password: '123456',
+            userCode: 'rfd14'
           }
         };
 
