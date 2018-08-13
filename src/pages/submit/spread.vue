348fd9941679e0038c4171a972b08c5413ef559b
sheng.lai@refordom.com <sheng.lai@reform.com>
2018-08-13 09:26:21 +0800

    bug

--- src/pages/submit/spread.vue
+++ src/pages/submit/spread.vue
@@ -58,7 +58,7 @@
             <x-textarea title="说明" :max="100" v-model="item.explain"></x-textarea>
         </group>
       </div>
-      <p class="note_tx" v-if="xp_list.length > 0 && formData==''">添加另一个 <span class="plus_tx" @click="plusType">类型</span> ? <span class="plus_delate" v-if="xp_list.length>1" @click="delateOne">删除</span></p>
+      <p class="note_tx" v-if="xp_list.length > 0 && formData">添加另一个 <span class="plus_tx" @click="plusType">类型</span> ? <span class="plus_delate" v-if="xp_list.length>1" @click="delateOne">删除</span></p>
     </div>
     <div class="s_btm vux-1px-t">
       <span class="count_part">合计:￥{{total | numberComma}}</span>
@@ -85,7 +85,7 @@ export default {
   data() {
     return {
       baseInfo: "",
-      formData: "",
+      formData: true,
       xp_list: [
         {
           name: "", //项目名称
@@ -417,6 +417,7 @@ export default {
           this.formDataNew = JSON.parse(res.tableContent[0].json_data);
           this.cacheData(this.formDataNew.transDetailUncalc);
           this.listDefault();
+          this.formData = false;
         })
         .catch(c => {
           console.log(c);
