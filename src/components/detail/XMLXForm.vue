e69da3df5382a542d66d85b474205851e1b92974
sheng.lai@refordom.com <sheng.lai@reform.com>
2018-08-16 17:38:06 +0800

    项目立项详情新增

--- src/components/detail/XMLXForm.vue
+++ src/components/detail/XMLXForm.vue
@@ -1,18 +1,83 @@
 <template>
   <div class="detail_wrapper xmrw-detail-container">
     <div class="basicPart">
+      <!-- 预算明细 -->
       <div class="trade_mode mg_auto box_sd">
-        <p class="title">项目名称</p>
-        <p class="mode">{{projectTask.projectName}}</p>
-      </div>
-      <!-- 实际情况 -->
-      <div class="trade_mode mg_auto box_sd">
-        <p class="title">实际情况</p>
+        <p class="title">预算明细</p>
         <group>
-          <cell title="实际完成日期" :value="projectTask.actualCompleteTime"></cell>
-          <cell title="实际工时" :value="projectTask.actualCompleteTime"></cell>
+          <cell title="预算收入" :value="approval.budgetIncome"></cell>
+          <cell title="预算成本" :value="approval.budgetCapital"></cell>
+          <cell title="预算费用" :value="approval.budgetCost"></cell>
+          <cell title="预算利润" :value="approval.budgetProfit"></cell>
+          <cell title="预算利润率" :value="approval.budgetProfitMargin"></cell>
+          <cell title="预算开始日期" :value="approval.expectStartDate | timeSplit"></cell>
+          <cell title="预算截至日期" :value="approval.expectEndDate | timeSplit"></cell>
         </group>
       </div>
+
+      <!-- 项目明细 -->
+      <div class="materiel_list mg_auto box_sd">
+        <div class="title">项目明细</div>
+        <div class="mater_list">
+          <div class="each_mater">
+            <div class="each_mater_wrapper ">
+              <div class="mater_main">
+                <!-- 项目名称 -->
+                <div class="mater_name">
+                  <span class="whiNum">项目名称</span>
+                 {{approval.projectName}}
+                </div>
+                <!-- 项目基本信息 -->
+                <div class="mater_info">
+                  <div class="withColor">
+                    <!-- 项目类型 -->
+                    <div class="ForInline" style="display:inline-block">
+                      <div class="mater_code">
+                        <span class="title">项目类型</span>
+                        <span class="num">{{approval.projectType}}</span>
+                      </div>
+                    </div>
+                    <!-- 项目经理 -->
+                    <div class="ForInline" style="display:inline-block">
+                      <div class="mater_spec">
+                        <span class="title">项目经理</span>
+                        <span class="num">{{approval.projectManager}}</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div class='mater_other'>
+                  <div class='mater_reimb'>
+                    <!-- 手机号码 -->
+                     <div class="price">
+                      <span class="title">手机号码: </span>
+                      <span class="content">
+                        {{approval.phoneNumber}}
+                      </span>
+                    </div>
+                    <!-- 项目说明 -->
+                    <div class="reason">
+                      <span class="title">项目说明: </span>
+                      <span class="content">
+                        {{approval.comment}}
+                      </span>
+                    </div>
+                    <!-- 项目备注 -->
+                    <div class="reason">
+                      <span class="title">项目备注: </span>
+                      <span class="content">
+                        {{comment.biComment || '无'}}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   </div>
 </template>
@@ -21,12 +86,18 @@
   import {Group, Cell, dateFormat,} from 'vux'
   import {getSOList,} from 'service/detailService'
   import detailCommon from 'components/mixins/detailCommon'
-  import {findProjectTask} from 'service/projectService'
+  import {findProjectTask,findProjectApproval} from 'service/projectService'
 
   export default {
     data() {
       return {
-        projectTask: {}
+        approval:'',
+        comment:'',
+      }
+    },
+    filters:{
+      timeSplit(val){
+        return val?val.split(' ')[0]:'';
       }
     },
     mixins: [detailCommon],
@@ -45,12 +116,9 @@
       },
       // 获取详情
       getOrderList(transCode = '') {
-        return findProjectTask(transCode).then(({formData = {}}) => {
-          let {projectTask} = formData;
-          this.projectTask = {
-            ...projectTask,
-            actualCompleteTime: dateFormat(projectTask.actualCompleteTime, 'YYYY-MM-DD')
-          };
+        return findProjectApproval(transCode).then(({formData = {}}) => {
+          this.approval = formData.approval;
+          this.comment = formData.comment;
         })
       },
     },
@@ -79,5 +147,8 @@
         }
       }
     }
+  }
+  .materiel_list .mater_list .each_mater .each_mater_wrapper .mater_main{
+    margin-left: 0;
   }
 </style>
