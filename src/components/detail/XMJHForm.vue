14d455183522acdfb459c3f6bfc2821477cfe62a
sheng.lai@refordom.com <sheng.lai@reform.com>
2018-08-17 18:27:56 +0800

    项目计划详情

--- src/components/detail/XMJHForm.vue
+++ src/components/detail/XMJHForm.vue
@@ -1,70 +1,95 @@
 <template>
   <div class="detail_wrapper xmrw-detail-container">
     <div class="basicPart">
-      <!-- 预算明细 -->
-      <div class="trade_mode mg_auto box_sd" v-for="(val,idx) in projectPlan" :key="idx">
-        <p class="title">项目计划明细</p>
+
+       <!-- 项目明细 -->
+      <div class="trade_mode mg_auto box_sd">
+        <p class="title">项目明细</p>
+        <group class="JH_mar_left">
+          <cell title="项目名称" :value="projectApproval.projectName"></cell>
+          <cell title="项目经理" :value="projectApproval.projectManager"></cell>
+          <cell title="项目说明" primary="content" align-items="flex-start" :value="projectApproval.comment"></cell>
+        </group>
+      </div>
+
+      <!-- 预算开始日期 -->
+      <div class="trade_mode mg_auto box_sd">
         <group>
-          <cell title="项目名称" :value="val.projectName"></cell>
-          <cell title="项目经理" :value="val.budgetCapital"></cell>
-          <cell title="项目说明" primary="content" align-items="flex-start" :value="val.budgetCost"></cell>
-          <cell title="预算开始日期" :value="val.expectStartDate | timeSplit"></cell>
-          <cell title="预算截至日期" :value="val.expectEndDate | timeSplit"></cell>
+          <cell title="预算开始日期" :value="projectApproval.expectStartDate | timeSplit"></cell>
+        </group>
+      </div>
+
+      <!-- 预算截止日期 -->
+      <div class="trade_mode mg_auto box_sd">
+        <group>
+          <cell title="预算截止日期" :value="projectApproval.expectEndDate | timeSplit"></cell>
+        </group>
+      </div>
+
+      <!-- 预算明细 -->
+      <!-- <div class="trade_mode mg_auto box_sd" v-for="(val,idx) in projectPlan" :key="idx">
+        <p class="title">任务计划明细</p>
+        <group>
           <cell title="任务名称" :value="val.taskName"></cell>
           <cell title="任务类型" :value="val.taskType"></cell>
           <cell title="任务说明" primary="content" align-items="flex-start" :value="val.comment"></cell>
           <cell title="计划工时" :value="val.planTime"></cell>
-          <cell title="截至日期" :value="val.deadline | timeSplit"></cell>
+          <cell title="截止日期" :value="val.deadline | timeSplit"></cell>
         </group>
-      </div>
+      </div> -->
 
-      <!-- 项目明细 -->
-      <!-- <div class="materiel_list mg_auto box_sd">
-        <div class="title">项目明细</div>
-        <div class="mater_list">
-          <div class="each_mater">
-            <div class="each_mater_wrapper ">
-              <div class="mater_main">
-                <div class="mater_name">
-                  <span class="whiNum">项目名称</span>
-                 {{projectPlan.projectName}}
-                </div>
-                <div class="mater_info">
-                  <div class="withColor">
-                    <div class="ForInline" style="display:inline-block">
-                      <div class="mater_code">
-                        <span class="title">项目类型</span>
-                        <span class="num">{{projectPlan.projectType}}</span>
-                      </div>
-                    </div>
-                    <div class="ForInline" style="display:inline-block">
-                      <div class="mater_spec">
-                        <span class="title">项目经理</span>
-                        <span class="num">{{projectPlan.projectManager}}</span>
-                      </div>
-                    </div>
+      <!-- 任务计划明细 -->
+      <div class="materiel_list mg_auto box_sd">
+        <p class="title">任务计划明细</p>
+        <div class="bor_bottom JH_mar_left" v-for="(item,index) in projectPlan" :key="'JH'+index">
+          <div class="mater_list">
+            <div class="each_mater JH_pad0">
+              <div class="each_mater_wrapper ">
+                <div class="mater_main">
+                  <div class="mater_name">
+                    <span class="whiNum">#{{index+1}}</span>
+                  {{item.taskName}}
                   </div>
-                </div>
-
-                <div class='mater_other'>
-                  <div class='mater_reimb'>
-                     <div class="price">
-                      <span class="title">手机号码: </span>
-                      <span class="content">
-                        {{projectPlan.phoneNumber}}
-                      </span>
+                  <!-- <div class="mater_info">
+                    <div class="withColor">
+                      <div class="ForInline" style="display:flex">
+                        <div class="mater_code">
+                          <span class="title">任务类型</span>
+                          <span class="num">{{item.taskType}}</span>
+                        </div>
+                        <div class="mater_spec">
+                          <span class="title">计划工时</span>
+                          <span class="num">{{item.planTime}}</span>
+                        </div>
+                      </div>
                     </div>
-                    <div class="reason">
-                      <span class="title">项目说明: </span>
-                      <span class="content">
-                        {{projectPlan.comment}}
+                  </div> -->
+                  <div class='mater_other'>
+                    <div class='mater_reimb'>
+                      <div class="reason">
+                        <span class="title">任务类型: </span>
+                        <span class="content">
+                          {{item.taskType}}
                       </span>
-                    </div>
-                    <div class="reason">
-                      <span class="title">项目备注: </span>
-                      <span class="content">
-                        {{comment.biComment || '无'}}
+                      </div>
+                      <div class="reason">
+                        <span class="title">计划工时: </span>
+                        <span class="content">
+                          {{item.planTime}}
                       </span>
+                      </div>
+                      <div class="reason">
+                        <span class="title">截止日期: </span>
+                        <span class="content">
+                          {{item.deadline | timeSplit}}
+                        </span>
+                      </div>
+                      <div class="reason">
+                        <span class="title">任务说明: </span>
+                        <span class="content">
+                          {{item.comment}}
+                      </span>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -72,8 +97,7 @@
             </div>
           </div>
         </div>
-      </div> -->
-
+      </div>
     </div>
   </div>
 </template>
@@ -89,6 +113,7 @@
       return {
         projectPlan:'',
         comment:'',
+        projectApproval:'',
       }
     },
     filters:{
@@ -102,19 +127,12 @@
       Cell,
     },
     methods: {
-      //选择默认图片
-      getDefaultImg(item) {
-        let url = require('assets/wl.png');
-        if (item) {
-          item.inventoryPic = url;
-        }
-        return url
-      },
       // 获取详情
       getOrderList(transCode = '') {
         return findProjectPlan(transCode).then(({formData = {}}) => {
           this.projectPlan = formData.projectPlan;
           this.comment = formData.comment;
+          this.projectApproval = formData.projectApproval;
         })
       },
     },
@@ -145,6 +163,37 @@
     }
   }
   .materiel_list .mater_list .each_mater .each_mater_wrapper .mater_main{
-    margin-left: 0;
-  }
+      margin-left: 0;
+      max-width: inherit;
+    }
+  .mater_other .reason{
+      display: flex;
+      .content{
+        flex: 1;
+      }
+    }
+    .bor_bottom{
+      position: relative;
+    }
+    .bor_bottom::after{
+      content: " ";
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      right: 0;
+      border-top: 1px solid #D9D9D9;
+      color: #D9D9D9;
+      transform-origin: 0 0;
+      transform: scaleY(0.5);
+    }
+    .bor_bottom:last-child()::after{
+      border:none;
+    }
+    .materiel_list .mater_list .JH_pad0{
+      padding: 0.04rem 0;
+    }
+    .JH_mar_left{
+      margin-left: 0.04rem;
+    }
 </style>
+
