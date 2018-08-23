4868babb93b0a51a427e4efdfc7adf3e95cc5064
刘治增 <zionstark.liu@refordom.com>
2018-08-23 14:48:51 +0800

    修改请求项目类产品的参数

--- src/service/saleRepotService.js
+++ src/service/saleRepotService.js
@@ -4,11 +4,7 @@ let saleReportService={
     //项目产品类列表
     saleRepotList(){
         return $axios.ajax({
-            url:'/ds/extr/fb14652e-6706-40f9-af82-35ffcfd205a0?page=1&start=0&limit=25',
-            // url: '/ds/extr/fb14652e-6706-40f9-af82-35ffcfd205a0',
-            data:{
-                sort:'%5B%7B%22property%22:%22trans_detail_uncalc.bankCharge%22,%22direction%22:%22ASC%22%7D%5D'
-            }
+            url:'/ds/extr/fb14652e-6706-40f9-af82-35ffcfd205a0?page=1&start=0&limit=25&sort=%5B%7B%22property%22:%22trans_detail_uncalc.bankCharge%22,%22direction%22:%22ASC%22%7D%5D',
         });
     },
     //提交产品合计
