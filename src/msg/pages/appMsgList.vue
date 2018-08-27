ef8a935974c38f8164d21ac26b910b8aa6ed46c9
刘治增 <zionstark.liu@refordom.com>
2018-08-27 16:27:17 +0800

    去除<消息列表>的已完成数据

--- src/msg/pages/appMsgList.vue
+++ src/msg/pages/appMsgList.vue
@@ -43,31 +43,6 @@
               <div class="info">{{item.nodeName}}</div>
             </div>
           </div>
-          <div class="todo_msg" v-else>
-            <div class="msg_top">
-              <!-- 表单状态 及 编码 -->
-              <div class="work_info">
-                <!-- 状态 -->
-                <span class="work_status " >{{item.status}}</span>
-                <!-- 编码 -->
-                <span class="work_code doing_code">{{item.transCode}}</span>
-              </div>
-              <!-- 时间 -->
-              <div class="time">
-                {{item | handleCrt}}
-              </div>
-            </div>
-            <!-- 上个节点审批信息 -->
-            <div class="handle_info vux-1px-b">
-              <div class="info">
-                <div class="handle_part">
-                  <span class="handle">已进行了</span>
-                  <span class="result" :class='{reject_c : item.nodeName === "拒绝" || item.nodeName === "撤回" || item.nodeName === "重新提交" }'>{{item.nodeName}}</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          
         </div>
     </r-scroll>
     <loading-form :show='showLoadding'></loading-form>
@@ -114,8 +89,7 @@ export default {
           let {isMyTask, nodeName} = tableContent[0];
           if (isMyTask === 1 && nodeName === '重新提交') {
             this.$router.push({
-              // path: `/list/${code}/fillform`,
-               path: `/notice/msglist/${code}/fillform`,
+              path: `/notice/msglist/${code}/fillform`,
               query: {
                 transCode: item.businessKey
               }
