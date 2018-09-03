0ea2f99cad8e9ce26fb2859e66d1618950cbcfa8
baixia123 <xia.bai@refordom.com>
2018-09-03 15:22:34 +0800

    更改采购入库,销售报价详情页样式

--- src/components/detail/CGRKForm.vue
+++ src/components/detail/CGRKForm.vue
@@ -10,36 +10,82 @@
               <span>其他应用里存在与本条相关联的数据，快去看看</span>
               <x-icon class="r_arw" type="ios-arrow-forward" size="16"></x-icon>
             </div>
+            <!-- 经办信息 （订单、主体等） -->
+            <div class="basic_info">
+              <!-- 订单编码栏 -->
+              <div class="serial_bar vux-1px-b">
+                <div>
+                  <span class="iconfont icon-dingdan1"></span>
+                  <span class="l_size_name">{{workFlowInfo.transCode && workFlowInfo.transCode.replace(/_/g,'')}}</span>
+                </div>
+                <p class="work_status" :class="workFlowInfo.dyClass">{{workFlowInfo.biStatus}}</p>
+              </div>
+              <!-- 经办信息 -->
+              <div class="handle_info vux-1px-b">
+                <div class="each_handle s_size_name">
+                  <span class="title">经办人：</span>
+                  <span class="content">{{orderInfo.handlerName}}</span>
+                </div>
+                <div class="each_handle s_size_name">
+                  <span class="title">经办主体：</span>
+                  <span class="content">{{orderInfo.handlerEntityName || "暂无"}}</span>
+                </div>
+              </div>
+              <!-- 创建时间 -->
+              <div class="crt_time s_size_name">
+                <span class="title">创建时间：</span>
+                <span class="content">{{orderInfo.crtTime}}</span>
+              </div>
+            </div>
             <!-- 工作流 -->
             <work-flow :work-flow-info="workFlowInfo" :full-work-flow="fullWL" :userName="userName" :is-my-task="isMyTask"
                       :no-status="orderInfo.biStatus"></work-flow>
-            <!-- 用户地址和基本信息-->
-            <div class="or_ads mg_auto box_sd">
-              <div class="user_info">
-                <span class="user_name">{{dealerInfo.dealerName}}</span>
+             <!-- 往来联系部分 交易基本信息-->
+            <div class="contacts_part">
+              <div class="main_content vux-1px-b">
+                <span class="iconfont icon-kehu"></span>
+                <div class="cp_name m_size_name">{{dealerInfo.dealerName}}</div>
+                <div class="other_info s_size_name">
+                  <span class="title">手机：</span>
+                  <span class="content">{{dealerInfo.dealerMobilePhone || '暂无'}}</span>
+                </div>
+                <div class="other_info s_size_name">
+                  <span class="title">地址：</span>
+                  <span class="content" v-if="dealerInfo.province">{{dealerInfo.province}}{{dealerInfo.city}}{{dealerInfo.county}}{{dealerInfo.address}}</span>
+                  <span class="content" v-else>{{dealerInfo.province}}{{dealerInfo.city}}{{dealerInfo.county}}{{dealerInfo.address || '暂无'}}</span>
+                </div>
               </div>
-              <div class="cp_info">
-                <p class="cp_name">{{dealerInfo.dealerMobilePhone}}</p>
-                <p class="cp_ads">
-                  {{dealerInfo.province}}{{dealerInfo.city}}{{dealerInfo.county}}{{dealerInfo.address}}</p>
+              <div class="other_content vux-1px-b">
+                <div class="trade_info s_size_name">
+                  <span class="title">结算方式：</span>
+                  <span class="mode">{{orderInfo.inPut.crDealerPaymentTerm || '无'}}</span>
+                </div>            
               </div>
-            </div>
-            <!-- 入库仓库 -->
-            <pop-warehouse-list title="仓库" :default-value="warehouse" disabled></pop-warehouse-list>
-            <!-- 结算方式 -->
-            <div class="trade_mode mg_auto box_sd">
-              <p class="title">结算方式</p>
-              <p class="mode">{{orderInfo.inPut.crDealerPaymentTerm || '无'}}</p>
-            </div>
-            <div class="trade_mode mg_auto box_sd">
-              <p class="title">创建时间</p>
-              <p class="mode">{{orderInfo.crtTime || '暂无'}}</p>
-            </div>
+              <!-- 仓库信息-->
+              <div class="main_content vux-1px-b">
+                <span class="iconfont icon--"></span>
+                <div class="cp_name m_size_name">{{warehouse.warehouseName}}</div>
+                <div class="other_info s_size_name">
+                  <span class="title">类型：</span>
+                  <span class="content">{{warehouse.warehouseType || '暂无'}}</span>
+                </div>
+                <div class="other_info s_size_name">
+                  <span class="title">地址：</span>
+                  <span class="content" v-if="warehouse.warehouseProvince">{{warehouse.warehouseProvince}}{{warehouse.warehouseCity}}{{warehouse.warehouseDistrict}}{{warehouse.warehouseAddress}} </span>
+                  <span class="content" v-else>{{warehouse.warehouseProvince}}{{warehouse.warehouseCity}}{{warehouse.warehouseDistrict}}{{warehouse.warehouseAddress || "暂无"}} </span>
+                </div>
+              </div>
+            </div> 
             <!-- 物料列表 -->
-            <div class="materiel_list mg_auto box_sd">
-              <div class="title">物料列表</div>
+            <div class="materiel_list">
+              <div class="title">
+                <span class="iconfont icon-Shape"></span>物料列表
+              </div>
               <div class="mater_list">
-                <div class="each_mater vux-1px-b" v-for="(item, index) in orderInfo.inPut.dataSet" :key='index'>
+                <div class="each_mater" 
+                  :class="{'vux-1px-b' : index !==  orderInfo.inPut.dataSet.length - 1}"
+                  v-for="(item, index) in orderInfo.inPut.dataSet" 
+                  :key='index'>
                   <div class="each_mater_wrapper">
                     <div class="mater_img">
                       <img :src="item.inventoryPic" alt="mater_img" @error="getDefaultImg(item)">
@@ -47,17 +93,17 @@
                     <div class="mater_main">
                       <!-- 物料名称 -->
                       <div class="mater_name">
-                        {{item.inventoryName_transObjCode}}
+                        {{item.inventoryName_transObjCode || '该物料未获取到名称，请检查物料信息。'}}
                       </div>
                       <!-- 物料基本信息 -->
                       <div class="mater_info">
-                        <!-- 物料编码、规格 -->
+                          <!-- 物料编码、规格 -->
                         <div class="withColor">
                           <!-- 物料编码 -->
                           <div class="ForInline" style="display:inline-block">
                             <div class="mater_code">
                               <span class="title">编码</span>
-                              <span class="num">{{item.transObjCode}}</span>
+                              <span class="num">{{item.transObjCode || '无'}}</span>
                             </div>
                           </div>
                           <!-- 物料规格 -->
@@ -79,21 +125,32 @@
                         <div class='mater_price'>
                           ￥{{item.tdAmount | toFixed | numberComma(3)}}
                           <span class="num"
-                                :style="{display:(item.tdAmount && item.tdAmount.toString().length >= 7 ? 'block' : '')}">
-                                    [金额: ￥{{item.noTaxAmount | toFixed | numberComma(3)}} + 税金: ￥{{item.taxAmount | toFixed | numberComma(3)}}]
-                                  </span>
+                                :style="{display:(item.tdAmount && item.tdAmount.toString().length >= 6 ? 'block' : '')}">
+                            [金额: ￥{{item.noTaxAmount | toFixed | numberComma(3)}} + 税金: ￥{{item.taxAmount | toFixed | numberComma(3)}}]
+                          </span>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
-                <!-- 金额合计栏 -->
-                <div class="price_list">
-                  <div class='title'>合计<span style="fontSize:.12rem;">(含税)</span></div>
-                  <div class="num"><span style="fontSize:.12rem;">￥</span>{{count | toFixed | numberComma(3)}}</div>
-                </div>
               </div>
             </div>
+            <!-- 金额明细 -->
+            <div class="price_detail">
+                <div class="price_list">
+                  <div class='title'>金额</div>
+                  <div class="num"><span class="symbol">￥</span>{{noTaxAmount | toFixed | numberComma(3)}}</div>
+                </div>
+                <div class="price_list">
+                  <div class='title'>税金</div>
+                  <div class="num"><span class="symbol">￥</span>{{taxAmount | toFixed | numberComma(3)}}</div>
+                </div>
+                <!-- 金额合计栏 -->
+                <div class="price_count vux-1px-t">
+                  <span class='title'>合计：</span>
+                  <span class="num"><span class="symbol">￥</span>{{count | toFixed | numberComma(3)}}</span>
+                </div>
+              </div>  
             <!-- 审批操作 -->
             <r-action :code="transCode" :task-id="taskId" :actions="actions"
               @on-submit-success="submitSuccessCallback"></r-action>
@@ -148,6 +205,24 @@ export default {
       warehouse: {},      // 入库仓库
       formViewUniqueId: 'e76a8c6f-05cc-45ee-ba93-299fe6751856'
     }
+  },
+  computed:{
+    // 合计金额
+    noTaxAmount() {
+      let total = 0;
+      this.orderInfo.inPut.dataSet.forEach(item => {
+        total = accAdd(total, item.noTaxAmount);
+      });
+      return total;
+    },
+    // 税金
+    taxAmount() {
+      let total = 0;
+      this.orderInfo.inPut.dataSet.forEach(item => {
+        total = accAdd(total, item.taxAmount);
+      });
+      return total;
+    },
   },
   mixins: [detailCommon],
   components: {
