9c99924c447c7382714b0b0768b74bc43fdd22bf
xuehongjie <jie.xue@refordom.com>
2018-09-05 18:19:18 +0800

    详情页增加物料组件，修改详情页物料列表组件，修改与物料相关的详情页

--- src/components/detail/BDKCDBForm.vue
+++ src/components/detail/BDKCDBForm.vue
@@ -13,8 +13,9 @@
             <!-- 经办信息 （订单、主体等） -->
             <basic-info :work-flow-info="workFlowInfo" :order-info="orderInfo"></basic-info>
             <!-- 工作流 -->
-            <work-flow :work-flow-info="workFlowInfo" :full-work-flow="fullWL" :userName="userName" :is-my-task="isMyTask"
-                      :no-status="orderInfo.biStatus"></work-flow>
+            <work-flow :work-flow-info="workFlowInfo" :full-work-flow="fullWL" :userName="userName"
+                       :is-my-task="isMyTask"
+                       :no-status="orderInfo.biStatus"></work-flow>
             <!-- 仓库信息 -->
             <div class="contacts_part">
               <!-- 出库-->
@@ -26,49 +27,18 @@
             <div class="materiel_list">
               <div class="title">物料列表</div>
               <div class="mater_list">
-                <div class="each_mater" :class="{'vux-1px-b' : index !==  orderInfo.inPut.dataSet.length - 1}"
-                     v-for="(item, index) in orderInfo.inPut.dataSet" :key='index'>
-                  <div class="each_mater_wrapper">
-                    <div class="mater_img">
-                      <img :src="item.inventoryPic" alt="mater_img" @error="getDefaultImg(item)">
-                    </div>
-                    <div class="mater_main">
-                      <!-- 物料名称 -->
-                      <div class="mater_name">
-                        {{item.inventoryName_transObjCode}}
-                      </div>
-                      <!-- 物料基本信息 -->
-                      <div class="mater_info">
-                        <!-- 物料编码、规格 -->
-                        <div class="withColor">
-                          <!-- 物料编码 -->
-                          <div class="ForInline" style="display:inline-block">
-                            <div class="mater_code">
-                              <span class="title">编码</span>
-                              <span class="num">{{item.transObjCode}}</span>
-                            </div>
-                          </div>
-                          <!-- 物料规格 -->
-                          <div class="ForInline" style="display:inline-block">
-                            <div class="mater_spec">
-                              <span class="title">规格</span>
-                              <span class="num">{{item.specification_transObjCode || '无'}}</span>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                      <!-- 调拨数量 -->
-                      <div class='mater_other'>
-                        <div class='mater_num'>
-                          <span class="num">
-                            <span class="symbol">调拨数量: </span>
-                            {{item.tdQty}}
-                          </span>
-                        </div>
-                      </div>
+                <matter-item :item="item" :class="{'vux-1px-b' : index !==  orderInfo.inPut.dataSet.length - 1}"
+                             v-for="(item, index) in orderInfo.inPut.dataSet" :key='index'>
+                  <!-- 调拨数量 -->
+                  <div class='mater_other' slot="other">
+                    <div class='mater_num'>
+                      <span class="num">
+                        <span class="symbol">调拨数量: </span>
+                        {{item.tdQty}}
+                      </span>
                     </div>
                   </div>
-                </div>
+                </matter-item>
               </div>
             </div>
             <!-- 审批操作 -->
@@ -81,7 +51,8 @@
                 <p class="vux-1px-b">相关实例</p>
               </div>
               <div class="relevant_list">
-                <div class="each_app vux-1px-b" v-for='(item,index) in RelatedAppList' :key="index" @click="getRelatedData(item)">
+                <div class="each_app vux-1px-b" v-for='(item,index) in RelatedAppList' :key="index"
+                     @click="getRelatedData(item)">
                   <div class="app_info">
                     <div class="title">业务应用</div>
                     <div class="app_name">
@@ -92,7 +63,7 @@
                       <span class="msg_tx">关联</span>
                     </div>
                     <div class="r_arrow" v-if='item.itemCount>0'>
-                      <x-icon type="ios-arrow-right" size="20" ></x-icon>
+                      <x-icon type="ios-arrow-right" size="20"></x-icon>
                     </div>
                   </div>
                 </div>
@@ -100,119 +71,125 @@
             </div>
           </div>
         </div>
-        <pop-related-list :show='showPop' :listId='listId' :filter='filtersData' v-model='showPop' @reload-page='reloadPage'></pop-related-list>
+        <pop-related-list :show='showPop' :listId='listId' :filter='filtersData' v-model='showPop'
+                          @reload-page='reloadPage'></pop-related-list>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-// 请求 引入
-import {getSOList,} from 'service/detailService'
-// mixins 引入
-import detailCommon from 'components/mixins/detailCommon'
-// 组件引入
-import RAction from 'components/RAction'
-import workFlow from 'components/workFlow'
-import PopWarehouseList from 'components/Popup/PopWarehouseList'
-import PopRelatedList from 'components/Popup/PopRelatedList'
-import WarehouseContent from 'components/detail/commonPart/WarehouseContent'
-export default {
-  data() {
-    return {
-      warehouseIn: {}, // 入库仓库详情
-      warehouseOut: {}, // 出库仓库详情
-      formViewUniqueId: 'a8c58e16-48f5-454e-98d8-4f8f9066e513'
-    }
-  },
-  mixins: [detailCommon],
-  components: {
-    workFlow,
-    PopWarehouseList,
-    RAction,
-    PopRelatedList,
-    WarehouseContent,
-  },
-  methods: {
-    //选择默认图片
-    getDefaultImg(item) {
-      let url = require('assets/wl_default02.png');
-      if (item) {
-        item.inventoryPic = url;
+  // 请求 引入
+  import {getSOList,} from 'service/detailService'
+  // mixins 引入
+  import detailCommon from 'components/mixins/detailCommon'
+  // 组件引入
+  import RAction from 'components/RAction'
+  import workFlow from 'components/workFlow'
+  import PopWarehouseList from 'components/Popup/PopWarehouseList'
+  import PopRelatedList from 'components/Popup/PopRelatedList'
+  import WarehouseContent from 'components/detail/commonPart/WarehouseContent'
+  import MatterItem from 'components/detail/commonPart/MatterItem'
+
+  export default {
+    data() {
+      return {
+        warehouseIn: {}, // 入库仓库详情
+        warehouseOut: {}, // 出库仓库详情
+        formViewUniqueId: 'a8c58e16-48f5-454e-98d8-4f8f9066e513'
       }
-      return url
     },
-    // 获取详情
-    getOrderList(transCode = '') {
-      return getSOList({
-        formViewUniqueId: this.formViewUniqueId,
-        transCode
-      }).then(data => {
-        // http200时提示报错信息
-        if (data.success === false) {
-          this.$vux.alert.show({
-            content: '抱歉，数据有误，暂无法查看',
-            onHide: () => {
-              this.$router.back();
-            }
-          });
-          return;
-        }
-        let {inPut = {}} = data.formData;
-        let {dataSet} = inPut;
-        for (let val of dataSet) {
-          val.inventoryPic = val.inventoryPic_transObjCode
-            ? `/H_roleplay-si/ds/download?url=${val.inventoryPic_transObjCode}&width=400&height=400`
-            : this.getDefaultImg();
-        }
-        // 入库
-        this.warehouseIn = {
-          warehouseCode: inPut.containerCode,
-          warehouseName: `${inPut.warehouseName_containerCode}(入库)`,
-          warehouseType: inPut.warehouseType_containerCode,
-          warehouseProvince: inPut.warehouseProvince_containerCode,
-          warehouseCity: inPut.warehouseCity_containerCode,
-          warehouseDistrict: inPut.warehouseDistrict_containerCode,
-          warehouseAddress: inPut.warehouseAddress_containerCode,
-        };
-        // 出库
-        this.warehouseOut = {
-          warehouseCode: inPut.containerCodeOut,
-          warehouseName: `${inPut.warehouseName_containerCodeOut}(出库)`,
-          warehouseType: inPut.warehouseType_containerCodeOut,
-          warehouseProvince: inPut.warehouseProvince_containerCodeOut,
-          warehouseCity: inPut.warehouseCity_containerCodeOut,
-          warehouseDistrict: inPut.warehouseDistrict_containerCodeOut,
-          warehouseAddress: inPut.warehouseAddress_containerCodeOut,
-        };
-        this.orderInfo = data.formData;
-        this.workFlowInfoHandler();
-      })
+    mixins: [detailCommon],
+    components: {
+      workFlow,
+      PopWarehouseList,
+      RAction,
+      PopRelatedList,
+      WarehouseContent,
+      MatterItem,
     },
-  },
-  created() {
+    methods: {
+      //选择默认图片
+      getDefaultImg(item) {
+        let url = require('assets/wl_default02.png');
+        if (item) {
+          item.inventoryPic = url;
+        }
+        return url
+      },
+      // 获取详情
+      getOrderList(transCode = '') {
+        return getSOList({
+          formViewUniqueId: this.formViewUniqueId,
+          transCode
+        }).then(data => {
+          // http200时提示报错信息
+          if (data.success === false) {
+            this.$vux.alert.show({
+              content: '抱歉，数据有误，暂无法查看',
+              onHide: () => {
+                this.$router.back();
+              }
+            });
+            return;
+          }
+          let {inPut = {}} = data.formData;
+          let {dataSet} = inPut;
+          for (let val of dataSet) {
+            val.inventoryPic = val.inventoryPic_transObjCode
+              ? `/H_roleplay-si/ds/download?url=${val.inventoryPic_transObjCode}&width=400&height=400`
+              : this.getDefaultImg();
+          }
+          // 入库
+          this.warehouseIn = {
+            warehouseCode: inPut.containerCode,
+            warehouseName: `${inPut.warehouseName_containerCode}(入库)`,
+            warehouseType: inPut.warehouseType_containerCode,
+            warehouseProvince: inPut.warehouseProvince_containerCode,
+            warehouseCity: inPut.warehouseCity_containerCode,
+            warehouseDistrict: inPut.warehouseDistrict_containerCode,
+            warehouseAddress: inPut.warehouseAddress_containerCode,
+          };
+          // 出库
+          this.warehouseOut = {
+            warehouseCode: inPut.containerCodeOut,
+            warehouseName: `${inPut.warehouseName_containerCodeOut}(出库)`,
+            warehouseType: inPut.warehouseType_containerCodeOut,
+            warehouseProvince: inPut.warehouseProvince_containerCodeOut,
+            warehouseCity: inPut.warehouseCity_containerCodeOut,
+            warehouseDistrict: inPut.warehouseDistrict_containerCodeOut,
+            warehouseAddress: inPut.warehouseAddress_containerCodeOut,
+          };
+          this.orderInfo = data.formData;
+          this.workFlowInfoHandler();
+        })
+      },
+    },
+    created() {
+    }
   }
-}
 </script>
 <style lang='scss' scoped>
   @import './../scss/bizDetail';
-  .contacts_part{
+
+  .contacts_part {
     margin-bottom: .1rem;
     padding: .06rem .1rem 0;
     width: 100%;
     background: #fff;
     box-sizing: border-box;
   }
+
   .mater_other {
-      .mater_num {
-        .num {
-          color: #5077aa;
-          font-size: .16rem;
-          font-weight: bold;
-          .symbol {
-            color: #757575;
-          }
+    .mater_num {
+      .num {
+        color: #5077aa;
+        font-size: .16rem;
+        font-weight: bold;
+        .symbol {
+          color: #757575;
         }
       }
+    }
   }
 </style>
--- src/components/detail/KCPDForm.vue
+++ src/components/detail/KCPDForm.vue
@@ -13,8 +13,9 @@
             <!-- 经办信息 （订单、主体等） -->
             <basic-info :work-flow-info="workFlowInfo" :order-info="orderInfo"></basic-info>
             <!-- 工作流 -->
-            <work-flow :work-flow-info="workFlowInfo" :full-work-flow="fullWL" :userName="userName" :is-my-task="isMyTask"
-                      :no-status="orderInfo.biStatus"></work-flow>
+            <work-flow :work-flow-info="workFlowInfo" :full-work-flow="fullWL" :userName="userName"
+                       :is-my-task="isMyTask"
+                       :no-status="orderInfo.biStatus"></work-flow>
             <!-- 仓库信息 -->
             <div class="contacts_part">
               <!-- 入库 -->
@@ -24,57 +25,27 @@
             <div class="materiel_list">
               <div class="title">物料列表</div>
               <div class="mater_list">
-                <div class="each_mater" :class="{'vux-1px-b' : index !==  orderInfo.inPut.dataSet.length - 1}"
-                     v-for="(item, index) in orderInfo.inPut.dataSet" :key='index'>
-                  <div class="each_mater_wrapper">
-                    <div class="mater_img">
-                      <img :src="item.inventoryPic" alt="mater_img" @error="getDefaultImg(item)">
-                    </div>
-                    <div class="mater_main">
-                      <!-- 物料名称 -->
-                      <div class="mater_name">
-                        {{item.inventoryName_transObjCode}}
-                      </div>
-                      <!-- 物料基本信息 -->
-                      <div class="mater_info">
-                        <!-- 物料编码、规格 -->
-                        <div class="withColor">
-                          <!-- 物料编码 -->
-                          <div class="ForInline" style="display:inline-block">
-                            <div class="mater_code">
-                              <span class="title">编码</span>
-                              <span class="num">{{item.transObjCode}}</span>
-                            </div>
-                          </div>
-                          <!-- 物料规格 -->
-                          <div class="ForInline" style="display:inline-block">
-                            <div class="mater_spec">
-                              <span class="title">规格</span>
-                              <span class="num">{{item.specification_transObjCode || '无'}}</span>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                      <!-- 物料数量和价格 -->
-                      <div class='mater_other'>
-                        <div class='mater_num'>
-                          <span class="num">
-                            <span class="symbol">盘点数量: </span>
-                            {{item.tdQty}}
-                          </span>
-                          <span class="num">
-                            <span class="symbol">差异数量: </span>
-                            {{item.differenceNum}}
-                          </span>
-                        </div>
-                      </div>
+                <matter-item :item="item" :class="{'vux-1px-b' : index !==  orderInfo.inPut.dataSet.length - 1}"
+                             v-for="(item, index) in orderInfo.inPut.dataSet" :key='index'>
+                  <!-- 物料数量和价格 -->
+                  <div class='mater_other' slot="other">
+                    <div class='mater_num'>
+                      <span class="num">
+                        <span class="symbol">盘点数量: </span>
+                        {{item.tdQty}}
+                      </span>
+                      <span class="num">
+                        <span class="symbol">差异数量: </span>
+                        {{item.differenceNum}}
+                      </span>
                     </div>
                   </div>
-                </div>
+                </matter-item>
               </div>
             </div>
             <!-- 审批操作 -->
-            <r-action :code="transCode" :task-id="taskId" :actions="actions" @on-submit-success="submitSuccessCallback"></r-action>
+            <r-action :code="transCode" :task-id="taskId" :actions="actions"
+                      @on-submit-success="submitSuccessCallback"></r-action>
           </div>
           <div class="swiper-slide" v-if='HasValRealted'>
             <div class='related_apply'>
@@ -82,7 +53,8 @@
                 <p class="vux-1px-b">相关实例</p>
               </div>
               <div class="relevant_list">
-                <div class="each_app vux-1px-b" v-for='(item,index) in RelatedAppList' :key="index" @click="getRelatedData(item)">
+                <div class="each_app vux-1px-b" v-for='(item,index) in RelatedAppList' :key="index"
+                     @click="getRelatedData(item)">
                   <div class="app_info">
                     <div class="title">业务应用</div>
                     <div class="app_name">
@@ -93,7 +65,7 @@
                       <span class="msg_tx">关联</span>
                     </div>
                     <div class="r_arrow" v-if='item.itemCount>0'>
-                      <x-icon type="ios-arrow-right" size="20" ></x-icon>
+                      <x-icon type="ios-arrow-right" size="20"></x-icon>
                     </div>
 
                   </div>
@@ -102,90 +74,95 @@
             </div>
           </div>
         </div>
-        <pop-related-list :show='showPop' :listId='listId' :filter='filtersData' v-model='showPop' @reload-page='reloadPage'></pop-related-list>
+        <pop-related-list :show='showPop' :listId='listId' :filter='filtersData' v-model='showPop'
+                          @reload-page='reloadPage'></pop-related-list>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-// 请求 引入
-import {getSOList,} from 'service/detailService'
-// mixins 引入
-import detailCommon from 'components/mixins/detailCommon'
-// 组件 引入
-import RAction from 'components/RAction'
-import workFlow from 'components/workFlow'
-import PopWarehouseList from 'components/Popup/PopWarehouseList'
-import PopRelatedList from 'components/Popup/PopRelatedList'
-import WarehouseContent from 'components/detail/commonPart/WarehouseContent'
-export default {
-  data() {
-    return {
-      warehouseIn: {}, // 入库仓库详情
-      formViewUniqueId: '064e455d-4277-473a-9e54-d9ae4b1f23be'
-    }
-  },
-  mixins: [detailCommon],
-  components: {
-    workFlow,
-    PopWarehouseList,
-    RAction,
-    PopRelatedList,
-    WarehouseContent,
-  },
-  methods: {
-    //选择默认图片
-    getDefaultImg(item) {
-      let url = require('assets/wl_default02.png');
-      if (item) {
-        item.inventoryPic = url;
+  // 请求 引入
+  import {getSOList,} from 'service/detailService'
+  // mixins 引入
+  import detailCommon from 'components/mixins/detailCommon'
+  // 组件 引入
+  import RAction from 'components/RAction'
+  import workFlow from 'components/workFlow'
+  import PopWarehouseList from 'components/Popup/PopWarehouseList'
+  import PopRelatedList from 'components/Popup/PopRelatedList'
+  import WarehouseContent from 'components/detail/commonPart/WarehouseContent'
+  import MatterItem from 'components/detail/commonPart/MatterItem'
+
+  export default {
+    data() {
+      return {
+        warehouseIn: {}, // 入库仓库详情
+        formViewUniqueId: '064e455d-4277-473a-9e54-d9ae4b1f23be'
       }
-      return url
     },
-    // 获取详情
-    getOrderList(transCode = '') {
-      return getSOList({
-        formViewUniqueId: this.formViewUniqueId,
-        transCode
-      }).then(data => {
-        // http200时提示报错信息
-        if (data.success === false) {
-          this.$vux.alert.show({
-            content: '抱歉，数据有误，暂无法查看',
-            onHide:()=>{
-              this.$router.back();
-            }
-          });
-          return;
-        }
-        let {inPut = {}} = data.formData;
-        let {dataSet} = inPut;
-        for (let val of dataSet) {
-          val.inventoryPic = val.inventoryPic_transObjCode
-            ? `/H_roleplay-si/ds/download?url=${val.inventoryPic_transObjCode}&width=400&height=400`
-            : this.getDefaultImg();
-        }
-        // 入库
-        this.warehouseIn = {
-          warehouseCode: inPut.containerCode,
-          warehouseName: inPut.warehouseName_containerCode,
-          warehouseType: inPut.warehouseType_containerCode,
-          warehouseProvince: inPut.warehouseProvince_containerCode,
-          warehouseCity: inPut.warehouseCity_containerCode,
-          warehouseDistrict: inPut.warehouseDistrict_containerCode,
-          warehouseAddress: inPut.warehouseAddress_containerCode,
-        };
-        this.orderInfo = data.formData;
-        this.workFlowInfoHandler();
-      })
+    mixins: [detailCommon],
+    components: {
+      workFlow,
+      PopWarehouseList,
+      RAction,
+      PopRelatedList,
+      WarehouseContent,
+      MatterItem,
     },
+    methods: {
+      //选择默认图片
+      getDefaultImg(item) {
+        let url = require('assets/wl_default02.png');
+        if (item) {
+          item.inventoryPic = url;
+        }
+        return url
+      },
+      // 获取详情
+      getOrderList(transCode = '') {
+        return getSOList({
+          formViewUniqueId: this.formViewUniqueId,
+          transCode
+        }).then(data => {
+          // http200时提示报错信息
+          if (data.success === false) {
+            this.$vux.alert.show({
+              content: '抱歉，数据有误，暂无法查看',
+              onHide: () => {
+                this.$router.back();
+              }
+            });
+            return;
+          }
+          let {inPut = {}} = data.formData;
+          let {dataSet} = inPut;
+          for (let val of dataSet) {
+            val.inventoryPic = val.inventoryPic_transObjCode
+              ? `/H_roleplay-si/ds/download?url=${val.inventoryPic_transObjCode}&width=400&height=400`
+              : this.getDefaultImg();
+          }
+          // 入库
+          this.warehouseIn = {
+            warehouseCode: inPut.containerCode,
+            warehouseName: inPut.warehouseName_containerCode,
+            warehouseType: inPut.warehouseType_containerCode,
+            warehouseProvince: inPut.warehouseProvince_containerCode,
+            warehouseCity: inPut.warehouseCity_containerCode,
+            warehouseDistrict: inPut.warehouseDistrict_containerCode,
+            warehouseAddress: inPut.warehouseAddress_containerCode,
+          };
+          this.orderInfo = data.formData;
+          this.workFlowInfoHandler();
+        })
+      },
+    }
   }
-}
 </script>
 
 <style lang='scss' scoped>
   @import './../scss/bizDetail';
+
   .contacts_part {
     margin-bottom: .1rem;
     padding: .06rem .1rem 0;
@@ -193,16 +170,17 @@ export default {
     background: #fff;
     box-sizing: border-box;
   }
+
   .mater_other {
-      .mater_num {
-        .num {
-          color: #5077aa;
-          font-size: .16rem;
-          font-weight: bold;
-          .symbol {
-            color: #757575;
-          }
+    .mater_num {
+      .num {
+        color: #5077aa;
+        font-size: .16rem;
+        font-weight: bold;
+        .symbol {
+          color: #757575;
         }
       }
+    }
   }
 </style>
