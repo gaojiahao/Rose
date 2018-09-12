884810505af3d0014284947d571a5dae52ef3022
刘治增 <zionstark.liu@refordom.com>
2018-09-12 11:08:46 +0800

    修改<FormCell>组件，新增文字靠右功能；修改物料列表底部的按钮文字

--- src/components/detail/commonPart/FormCell.vue
+++ src/components/detail/commonPart/FormCell.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-if="cellContent" class="each_cell" :class="{'vux-1px-t' : showTopBorder}">
     <span class="cell_name">{{ cellTitle }}:</span>
-    <span class="cell_content">
+    <span class="cell_content" :class="{'when-is-right' : textRight}">
       <span class="symbol" v-if="showSymbol">￥</span>{{ cellContent }}
     </span>
   </div>
@@ -17,7 +17,11 @@ export default {
       type: Boolean,
       default: true
     },
-    showSymbol: {                   // 展示 ￥
+    textRight: {                    // 文字居右
+      type: Boolean,
+      default: false
+    },
+    showSymbol: {                   // 展示'￥'符号
       type: Boolean,
       default: false
     }
@@ -27,8 +31,10 @@ export default {
 
 <style lang='scss' scoped>
 .each_cell {
+  display: flex;
   font-size: .14rem;
   padding: .08rem 0;
+  align-items: center;
   .cell_name {
     color: #757575;
     margin-right: .1rem;
@@ -37,5 +43,10 @@ export default {
     line-height: .28rem;
     font-weight: bold;
   }
+  .when-is-right {
+    flex: 1;
+    text-align: right;
+  }
 }
+
 </style>
