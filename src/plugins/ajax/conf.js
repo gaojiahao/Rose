7a9d85fa31c0f2e43a72fbbe87ec270e4fe5ad24
xuehongjie <jie.xue@refordom.com>
2018-09-14 14:03:05 +0800

    修改请求baseURL

--- src/plugins/ajax/conf.js
+++ src/plugins/ajax/conf.js
@@ -7,11 +7,13 @@ export const corpid = 'wx5311bd8608c14d98'; // 企业id
 export const corpsecret = 'EvHlK-H-KKz3Wra3x_E6UTavEiBdTs2_9B9LEhmXJyM'; // 应用凭证
 export const agentid = '1000004'; // 企业应用的id
 export const redirect_uri = 'https%3a%2f%2fwww.gofuit.com%2fRose'; // 首页路径
+export const baseURL = '/H_roleplay-si'; // 根目录
 
 export default {
   corpid,
   corpsecret,
   agentid,
   redirect_uri,
+  baseURL,
 };
 
