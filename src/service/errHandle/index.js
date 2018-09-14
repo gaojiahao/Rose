059736eba932c6648aaf1c9d28613a36b63b0712
刘治增 <zionstark.liu@refordom.com>
2018-09-14 11:38:58 +0800

    修改<库存盘点>提交的字段文字；修改<错误弹窗>的提醒判断

--- src/service/errHandle/index.js
+++ src/service/errHandle/index.js
@@ -3,7 +3,7 @@ import {AlertModule} from 'vux'
 
 // 错误处理回调
 export default function errorHandler(err){
-  if(err && err.includes('查看数据错误')){
+  if(err && err.includes('查看数据错误') || ('database')){
     err = '查看数据失败，请联系IT服务商'
   }
   // 弹窗提醒
