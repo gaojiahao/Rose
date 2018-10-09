6fe5d0c042991c6bdbbf5f6fa89b463025f9df2c
baixia123 <xia.bai@refordom.com>
2018-10-09 17:48:53 +0800

    引入物料组件使用

--- src/components/apply/production/NBJGLLForm.vue
+++ src/components/apply/production/NBJGLLForm.vue
@@ -30,43 +30,15 @@
                   <span class="order_num">{{key}}</span>
                 </div>
                 <div :class="{mater_delete : matterModifyClass}" v-for="(item, index) in oItem" :key="index">
-                  <div class="each_mater_wrapper">
-                    <div class="order-matter">
-                      <div class="mater_img">
-                        <img :src="item.inventoryPic" alt="mater_img" @error="getDefaultImg(item)">
-                      </div>
-                      <div class="mater_main">
-                        <!-- 物料名称 -->
-                        <div class="mater_name">
-                          {{item.inventoryName}}
-                        </div>
-                        <!-- 物料基本信息 -->
-                        <div class="mater_info">
-                          <!-- 物料编码、规格 -->
-                          <div class="withColor">
-                            <!-- 物料编码 -->
-                            <div class="ForInline" style="display:inline-block">
-                              <div class="mater_code">
-                                <span class="title">编码</span>
-                                <span class="num">{{item.inventoryCode}}</span>
-                              </div>
-                            </div>
-                            <!-- 物料规格 -->
-                            <div class="ForInline" style="display:inline-block">
-                              <div class="mater_spec">
-                                <span class="title">规格</span>
-                                <span class="num">{{item.specification || '无'}}</span>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                        <!-- 物料属性和单位 -->
-                        <div class='mater_more'>
-                          <span>单位: {{item.measureUnit}}</span>
+                  <matter-item :item="item" @on-modify="modifyMatter(item,index, key)" :show-delete="matterModifyClass">
+                    <template slot-scope="{item}" slot="info">
+                      <!-- 物料属性和单位 -->
+                      <div class='mater_more'>
+                        <span>单位: {{item.measureUnit}}</span>
                           <span>待领料: {{item.qtyBal}}</span>
                           <span>可用库存: {{item.qtyStock}}</span>
-                        </div>
-                        <div class="mater_other">
+                      </div>
+                      <div class="mater_other">
                           <div class="matter-remain" v-if="item.warehouseName">
                             <i class="iconfont icon--"></i>
                             <span>{{item.warehouseName}}</span>
@@ -75,13 +47,8 @@
                             <span class="symbol">本次领料: </span>{{item.tdQty}}
                           </div>
                         </div>
-                        <!-- 编辑图标 -->
-                        <div class="edit-part vux-1px-l" @click="modifyMatter(item, index, key)" v-show="!matterModifyClass">
-                          <span class='iconfont icon-bianji1'></span>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+                    </template>
+                  </matter-item>
                   <div class='delete_icon' @click="delClick(index, item, key)" v-if='matterModifyClass'>
                     <x-icon type="ios-checkmark" size="20" class="checked" v-show="showSelIcon(item)"></x-icon>
                     <x-icon type="ios-circle-outline" size="20" v-show="!showSelIcon(item)"></x-icon>
@@ -155,12 +122,13 @@ import RNumber from 'components/RNumber'
 import PopWarehouseList from 'components/Popup/PopWarehouseList'
 import PopMatter from 'components/apply/commonPart/MatterPop'
 import PopOrderList from 'components/Popup/PopOrderList'
+import MatterItem from 'components/apply/commonPart/MatterItem'
 export default {
   mixins: [ApplyCommon],
   components: {
     Icon, Cell, Group, XInput,
     RNumber,
-    PopOrderList, PopWarehouseList, PopMatter,
+    PopOrderList, PopWarehouseList, PopMatter,MatterItem
   },
   data() {
     return {
