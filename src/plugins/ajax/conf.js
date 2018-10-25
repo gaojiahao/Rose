76e266620d3d7ef76407b21abed764f6787611bf
刘治增 <zionstark.liu@refordom.com>
2018-10-25 19:45:23 +0800

    新增七感环境的配置

--- src/plugins/ajax/conf.js
+++ src/plugins/ajax/conf.js
@@ -10,11 +10,17 @@
 // export const redirect_uri = 'http%3a%2f%2fcpacn2012.3322.org%3a1175%2fHermes'; // 首页路径
 
 // 七感
-export const corpid = 'wwa7ec2d7f7c4390a1'; // 企业id
-export const secret = '9mPAeKBUdLju-roDzZFo2E2LzbgdrnSuLQ46dr_xutI'; // 应用凭证
-export const agentid = '1000002'; // 企业应用的id
-export const redirect_uri = 'https%3a%2f%2f7senses.roletask.com%2fHermes'; // 首页路径
+// export const corpid = 'wwa7ec2d7f7c4390a1'; // 企业id
+// export const secret = '9mPAeKBUdLju-roDzZFo2E2LzbgdrnSuLQ46dr_xutI'; // 应用凭证
+// export const agentid = '1000002'; // 企业应用的id
+// export const redirect_uri = 'https%3a%2f%2f7senses.roletask.com%2fHermes'; // 首页路径
 
+// ZooCoffee
+
+export const corpid = 'ww16b87431cd6fb4f1'; // 企业id
+export const secret = '4qeG9ge1wNjxBP6PBgqo01pG--RSWrZBGasBc57IMGY'; // 应用凭证
+export const agentid = '1000002'; // 企业应用的id
+export const redirect_uri = 'https%3a%2f%2fZooCoffee.roletask.com%2fHermes'; // 首页路径
 export default {
   corpid,
   secret,
