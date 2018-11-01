f1d692f918dea5a6d3a916c783b827f949d0db30
baixia123 <xia.bai@refordom.com>
2018-11-01 18:51:00 +0800

    修改相关实例样式

--- src/home/pages/apps/bizApp/detail/related/related.vue
+++ src/home/pages/apps/bizApp/detail/related/related.vue
@@ -1,61 +1,40 @@
 <template>
   <div class='pages related_apply'>
     <div class="big_title">
-      <p class="vux-1px-b">相关实例</p>
+      <p class="">相关实例</p>
     </div>
-    <!--<div class="relevant_list">
-      <div class="each_app vux-1px-b" v-for='(item, index) in RelatedAppList' :key="index"
-           @click="getRelatedData(item)">
-        <div class="app_info">
-          <div class="title">业务应用</div>
-          <div class="app_name">
-            <span>{{item.listName}}</span>
-          </div>
-          <div class="msg_num">
-            {{item.itemCount}}
-            <span class="msg_tx">关联</span>
-          </div>
-          <div class="r_arrow" v-if='item.itemCount>0'>
-            <x-icon type="ios-arrow-right" size="20"></x-icon>
-          </div>
-        </div>
-      </div>
-    </div>-->
-    <div class="swiper-container swiper-related">
+    <div class="swiper-container swiper_related">
       <div class="swiper-wrapper">
         <div class="swiper-slide" v-for="(item, index) in RelatedAppList" :key="index">
-          <header class="header">
-            <div>
+          <div class="header vux-1px-b">
+            <div class="related_info">
               <img class="icon" :src="item.icon">
-              <span class="related-name">{{item.listName}}</span>
+              <div>
+                <div class="related_name">{{item.listName}}</div>
+                <div class="related_num">实例数量：<span>{{item.itemCount}}</span></div>
+              </div> 
             </div>
-            <div @click="goRelation(item)">创建并关联</div>
-          </header>
-          <div class="related-list">
-            <div class="related-item" v-for="(rItem, rIndex) in item.relatedList" @click="goDetail(item, rItem)"
+            <div @click="goRelation(item)" class="create_order">创建并关联</div>
+          </div>
+          <div class="related_list">
+            <div class="related_item vux-1px-b" v-for="(rItem, rIndex) in item.relatedList" @click="goDetail(item, rItem)"
                  :key="rIndex">
-              <div class="code">
-                <span class="order_title">编码</span>
-                <span class="order_num">{{rItem.transCode}}</span>
+              <div>{{rItem.transCode}}</div>
+              <div class="creator_name">{{rItem.creatorName}}
+                <span>[创建人]</span>
+                <span class="iconfont icon-youjiantou"></span>
               </div>
-              <div class="app-name">{{rItem.transTypeName}}</div>
-              <div>创建人: {{rItem.creatorName}}</div>
-              <div>创建时间: {{rItem.crtTime | dateFormat('YYYY-MM-DD')}}</div>
-              <div>生效时间: {{rItem.effectiveTime | dateFormat('YYYY-MM-DD')}}</div>
             </div>
           </div>
         </div>
       </div>
     </div>
-    <pop-related-list :show='true' :idInfo='idInfo' :filtersData='filtersData' v-model='showPop'
-                      ref='relatedList'></pop-related-list>
   </div>
 
 </template>
 
 <script>
   import {dateFormat} from 'vux'
-  import PopRelatedList from 'components/Popup/PopRelatedList'
   import {getAppExampleDetails} from 'service/detailService'
   import {getList} from 'service/commonService'
   import {getListView} from 'service/detailService'
@@ -64,33 +43,13 @@
   export default {
     data() {
       return {
-        showPop: false,
         listId: '',
         transCode: '',
-        idInfo: {},                     // 当中包括 fileId(文件夹(应用分类)id) listId(应用id)
-        filtersData: [],
         RelatedAppList: [],
-        activeIndex: -1,
         swiper: null,
       }
     },
-    components: {
-      PopRelatedList
-    },
     methods: {
-      //显示相关实例的pop
-      getRelatedData(item) {
-        if (item.itemCount > 0) {
-          this.idInfo = {
-            fileId: item.parentNav,
-            listId: item.listId
-          }
-          item.content.forEach(val => {
-            this.filtersData.push(val.transCode);
-          })
-          this.$HandleLoad.show();
-        }
-      },
       //TODO 获取相关实例
       getAppExampleDetails() {
         return getAppExampleDetails({
@@ -177,7 +136,7 @@
         });
         Promise.all(promises).then(() => {
           this.$nextTick(() => {
-            this.swiper = new this.Swiper('.swiper-related', {
+            this.swiper = new this.Swiper('.swiper_related', {
               slidesPerView: 'auto',
               centeredSlides: true,
               observer: true,       //修改swiper自己或子元素时，自动初始化swiper
@@ -197,42 +156,76 @@
   }
 
   .related_apply {
-    background: #fff;
-    .swiper-related {
+    background: #f8f8f8;
+    .swiper_related {
       width: 100%;
-      height: calc(100% - .56rem);
+      height: 2.5rem;
       .swiper-slide {
-        padding: .1rem;
         width: 90%;
         margin: 0 .05rem;
-        border-radius: .2rem;
-        background: #f0f1f5;
+        border-radius: .1rem;
+        background: #fff;
         box-sizing: border-box;
       }
       .header {
         display: flex;
         justify-content: space-between;
         height: .54rem;
+        padding: 0.1rem;
+        .related_info{
+          display: flex;
+          .icon {
+            margin-right: .1rem;
+            width: .54rem;
+            height: .54rem;
+            border-radius: 0.08rem;
+          }
+          .related_name {
+            font-weight: bold;
+            color:#111;
+          }
+          .related_num{
+            font-size: 0.12rem;
+            color: #757575;
+            span{
+              color: #c93d1b;
+              font-weight: bold;
+            }
+          }
+        }
+        .create_order{
+          color: #4F90F9;
+          font-size: 0.14rem;
+          vertical-align: middle;
+          line-height: 0.54rem;
+        }
       }
-      .icon {
-        margin-right: .1rem;
-        width: .54rem;
-        height: .54rem;
-      }
-      .related-name {
-        vertical-align: top;
-      }
-      .related-list {
+      
+      .related_list {
         height: calc(100% - .54rem);
+        padding-bottom: 0.2rem;
+        box-sizing: border-box;
         overflow: auto;
       }
-      .related-item {
-        margin: .1rem auto .2rem;
-        padding: .06rem .08rem .04rem;
-        border-radius: .08rem;
-        background-color: #fff;
-        box-shadow: 0 2px 10px #e8e8e8;
+      .related_item {
+        margin: 0 .1rem;
+        padding: 0.1rem 0;
         box-sizing: border-box;
+        display: flex;
+        font-size: 0.14rem;
+        justify-content: space-between;
+        font-weight: bold;
+        .creator_name{
+          font-weight: normal;
+          span{
+            font-size: 0.1rem;
+            color: #7d7d7d;
+            &.icon-youjiantou{
+              font-size: 0.13rem;
+              margin-left: 0.05rem;
+            }
+          }
+        }
       }
       //实例编码
       .code {
@@ -269,47 +262,6 @@
     font-size: .3rem;
     font-weight: 300;
     box-sizing: border-box;
-  }
-
-  // 相关应用
-  .relevant_list {
-    width: 90%;
-    margin: 0 auto;
-    .each_app {
-      margin-bottom: .1rem;
-      position: relative;
-      border-radius: .08rem;
-      .app_info {
-        padding: .1rem;
-        box-sizing: border-box;
-        .title {
-          color: #4F90F9;
-          font-size: .12rem;
-          font-weight: bold;
-        }
-        .app_name {
-          font-size: .18rem;
-          .msg_count {
-            float: right;
-            margin-right: .1rem;
-          }
-        }
-        .msg_num {
-          color: #3f72af;
-          font-size: .2rem;
-          .msg_tx {
-            // color: #757575;
-            font-size: .12rem;
-          }
-        }
-        .r_arrow {
-          position: absolute;
-          right: 0;
-          top: 50%;
-          transform: translate(0, -50%);
-        }
-      }
-    }
   }
 
 </style>
