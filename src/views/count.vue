c8144db42bc4ac20944a2afc4dd6b2290644648e
xuehongjie <jie.xue@refordom.com>
2018-11-01 19:07:55 +0800

    提交页面增加住宿费等金额字段，修改展示界面

--- src/views/count.vue
+++ src/views/count.vue
@@ -1,41 +1,42 @@
 <template>
   <div class="pages pd_btm">
     <div class="mescroll">
-        <group class="count_part">
+      <group class="count_part">
         <cell title="最终合计" :value="total4 | numberComma"></cell>
         <cell-form-preview class="count_dt_part" :list="list4"></cell-form-preview>
-        </group>
+      </group>
 
-        <load-more tip="项目类产品" :show-loading='false'></load-more>
-        <form-preview 
-            class="project_part"
-            header-label="金额" 
-            :header-value="total1 | numberComma" 
-            :body-items="list1">
-        </form-preview>
+      <load-more tip="项目类产品" :show-loading='false'></load-more>
+      <form-preview
+        class="project_part"
+        header-label="金额"
+        :header-value="total1 | numberComma"
+        :body-items="list1">
+      </form-preview>
 
-        <load-more tip="A类产品" :show-loading='false'></load-more>
-        <form-preview 
-            class="project_part"
-            header-label="金额" 
-            :header-value="total2 | numberComma">
-        </form-preview>
+      <load-more tip="A类产品" :show-loading='false'></load-more>
+      <form-preview
+        class="project_part"
+        header-label="金额"
+        :header-value="total2 | numberComma"
+        :body-items="listSalesAmt">
+      </form-preview>
 
-        <load-more tip="B类产品" :show-loading='false'></load-more>
-        <form-preview 
-            class="project_part"
-            header-label="金额" 
-            :header-value="total3 | numberComma">
-        </form-preview>
+      <load-more tip="B类产品" :show-loading='false'></load-more>
+      <form-preview
+        class="project_part"
+        header-label="金额"
+        :header-value="total3 | numberComma">
+      </form-preview>
     </div>
 
 
-    <x-button 
-        id="submit_button" 
-        :gradients="['#B99763', '#E7D0A2']" 
-        @click.native="sendOrder"
-        >
-        确定提交
+    <x-button
+      id="submit_button"
+      :gradients="['#B99763', '#E7D0A2']"
+      @click.native="sendOrder"
+    >
+      确定提交
     </x-button>
     <alert></alert>
     <confirm></confirm>
@@ -43,63 +44,64 @@
 </template>
 
 <script>
-import saleRepotService from "service/saleRepotService";
-import {
-  Cell,
-  Alert,
-  Group,
-  Confirm,
-  Divider,
-  XButton,
-  LoadMore,
-  FormPreview,
-  numberComma,
-  querystring,
-  CellFormPreview
-} from "vux";
-
-export default {
-  components: {
-    Alert,
+  import saleRepotService from "service/saleRepotService";
+  import {
     Cell,
+    Alert,
     Group,
+    Confirm,
     Divider,
     XButton,
-    Confirm,
     LoadMore,
     FormPreview,
+    numberComma,
+    querystring,
     CellFormPreview
-  },
-  data() {
-    return {
-      list1: [],
-      list2: [],
-      list3: [],
-      list4: [],
-      total1: "",
-      total2: "",
-      total3: "",
-      total4: "",
-      childInfo: ""
-    };
-  },
-  filters: {
-    numberComma
-  },
-  methods: {
-    /*
-         * 提交数据
-         */
+  } from "vux";
 
-    sendOrder() {
-      let that = this;
-      this.$vux.confirm.show({
-        title: "温馨提示",
-        content: "请确认提交数据是否正确？",
-        confirmText: "我已确认",
-        cancelText: "再去看看",
-        onConfirm() {
-          saleRepotService.subAmount(querystring.stringify(that.childInfo)).then(data => {
+  export default {
+    components: {
+      Alert,
+      Cell,
+      Group,
+      Divider,
+      XButton,
+      Confirm,
+      LoadMore,
+      FormPreview,
+      CellFormPreview
+    },
+    data() {
+      return {
+        list1: [],
+        list2: [],
+        list3: [],
+        list4: [],
+        total1: "",
+        total2: "",
+        total3: "",
+        total4: "",
+        childInfo: "",
+        listSalesAmt: [],
+      };
+    },
+    filters: {
+      numberComma
+    },
+    methods: {
+      /*
+           * 提交数据
+           */
+
+      sendOrder() {
+        let that = this;
+        this.$vux.confirm.show({
+          title: "温馨提示",
+          content: "请确认提交数据是否正确？",
+          confirmText: "我已确认",
+          cancelText: "再去看看",
+          onConfirm() {
+            saleRepotService.subAmount(querystring.stringify(that.childInfo)).then(data => {
               if (data.success) {
                 that.$vux.alert.show({
                   title: "提示",
@@ -113,98 +115,118 @@ export default {
                 });
               }
             })
-            .catch(data => {
-              if (data.success == false) {
-                that.$vux.alert.show({
-                  title: "提示",
-                  content: data.message
-                });
-              }
+              .catch(data => {
+                if (data.success == false) {
+                  that.$vux.alert.show({
+                    title: "提示",
+                    content: data.message
+                  });
+                }
+              });
+          }
+        });
+      },
+      //获取数据
+      info() {
+        let total1 = 0, // 项目类产品金额
+          total2 = 0, // A类金额
+          total3 = 0, // B类金额
+          list4_num = 0; // 最终合计 数量
+
+        //从缓存中拿到上个页面提交的数据
+        let jsonData = JSON.parse(this.childInfo.jsonData);
+        let {transDetailUncalc, baseinfoExt = {}} = jsonData;
+        console.log(baseinfoExt)
+
+        for (let i = 0; i < transDetailUncalc.length; i++) {
+          if (
+            transDetailUncalc[i].containerCode === "项目类产品" &&
+            transDetailUncalc[i].transObjCode
+          ) {
+            //项目类产品明细
+            this.list1.push({
+              label: transDetailUncalc[i].transObjCode,
+              value: `${transDetailUncalc[i].qty}件/套`
             });
+            // 最终合计 数量
+            list4_num += transDetailUncalc[i].qty == '' ? 0 : transDetailUncalc[i].qty;
+
+            this.list4 = [
+              {
+                label: "合计数量",
+                value: `${list4_num}件/套`
+              }
+            ];
+            //项目类产品金额汇总
+            total1 += transDetailUncalc[i].amount;
+          } else if (transDetailUncalc[i].containerCode == "A") {
+            this.list2.push({
+              label: transDetailUncalc[i].transObjCode,
+              value: transDetailUncalc[i].qty
+            });
+            total2 = transDetailUncalc[i].amount;
+          } else if (transDetailUncalc[i].containerCode == "B") {
+            this.list3.push({
+              label: transDetailUncalc[i].transObjCode,
+              value: transDetailUncalc[i].qty
+            });
+            total3 = transDetailUncalc[i].amount;
+          }
         }
-      });
-    },
-    //获取数据
-    info() {
-      let total1 = 0, // 项目类产品金额
-        total2 = 0, // A类金额
-        total3 = 0, // B类金额
-        list4_num = 0; // 最终合计 数量
-
-      //从缓存中拿到上个页面提交的数据
-      let jsonData = JSON.parse(this.childInfo.jsonData).transDetailUncalc;
-
-      for (let i = 0; i < jsonData.length; i++) {
-        if (
-          jsonData[i].containerCode === "项目类产品" &&
-          jsonData[i].transObjCode
-        ) {
-          //项目类产品明细
-          this.list1.push({
-            label: jsonData[i].transObjCode,
-            value: `${jsonData[i].qty}件/套`
-          });
-          // 最终合计 数量
-          list4_num += jsonData[i].qty==''?0:jsonData[i].qty;
-
-          this.list4 = [
-            {
-              label: "合计数量",
-              value: `${list4_num}件/套`
-            }
-          ];
-          //项目类产品金额汇总
-          total1 += jsonData[i].amount;
-        } else if (jsonData[i].containerCode == "A") {
-          this.list2.push({
-            label: jsonData[i].transObjCode,
-            value: jsonData[i].qty
-          });
-          total2 = jsonData[i].amount;
-        } else if (jsonData[i].containerCode == "B") {
-          this.list3.push({
-            label: jsonData[i].transObjCode,
-            value: jsonData[i].qty
-          });
-          total3 = jsonData[i].amount;
-        }
+        this.total1 = "￥" + total1;
+        this.total2 = "￥" + total2;
+        this.total3 = "￥" + total3;
+        let num = Number(total2) + Number(total3);
+        this.total4 = "￥" + num;
+        this.listSalesAmt = [
+          {
+            label: '住宿费',
+            value: baseinfoExt.double7,
+          }, {
+            label: '其他费用',
+            value: baseinfoExt.double8,
+          }, {
+            label: '合计',
+            value: baseinfoExt.double9,
+          }, {
+            label: '费用销量比',
+            value: baseinfoExt.varchar13,
+          },
+        ]
       }
-      this.total1 = "￥" + total1;
-      this.total2 = "￥" + total2;
-      this.total3 = "￥" + total3;
-      let num = Number(total2) + Number(total3);
-      this.total4 = "￥" + num;
+    },
+    mounted() {
+      this.childInfo = JSON.parse(
+        localStorage.getItem("saleReportInfo")
+      ).saleReportRemark;
+      this.info();
     }
-  },
-  mounted() {
-    this.childInfo = JSON.parse(
-      localStorage.getItem("saleReportInfo")
-    ).saleReportRemark;
-    this.info();
-  }
-};
+  };
 </script>
 
 <style>
-.mescroll {
-  width: 100%;
-  padding-bottom: 50px;
-}
-/* 提交订单按钮  */
-#submit_button {
-  width: 100%;
-  position: fixed;
-  left: 0;
-  bottom: 0;
-  border-radius: 0;
-  margin-top: 10px;
-}
-.pd_btm .weui-cells {
-  width: 100%;
-  box-sizing: border-box;
-}
-.pd_btm .project_part {
-  width: 100%;
-  box-sizing: border-box;
-}
+  .mescroll {
+    width: 100%;
+    padding-bottom: 50px;
+  }
+
+  /* 提交订单按钮  */
+  #submit_button {
+    width: 100%;
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    border-radius: 0;
+    margin-top: 10px;
+  }
+
+  .pd_btm .weui-cells {
+    width: 100%;
+    box-sizing: border-box;
+  }
+
+  .pd_btm .project_part {
+    width: 100%;
+    box-sizing: border-box;
+  }
 </style>
--- src/views/my_record.vue
+++ src/views/my_record.vue
@@ -1,160 +1,191 @@
 <template>
   <div class="pages">
     <tab :line-width=2 active-color='#B99763'>
-        <tab-item 
-            class="vux-center" 
-            :selected="demo2 === item" 
-            v-for="(item, index) in listTab" 
-            @click.native="mylist(index)" 
-            :key="index"
-            >
-            {{item}}
-        </tab-item>
+      <tab-item
+        class="vux-center"
+        :selected="demo2 === item"
+        v-for="(item, index) in listTab"
+        @click.native="mylist(index)"
+        :key="index"
+      >
+        {{item}}
+      </tab-item>
     </tab>
     <div class="my_info_part">
-        <divider>项目类产品</divider>
-        <form-preview 
-            class="project_part"
-            header-label="金额" 
-            :header-value="'￥'+list1Total | numberComma" 
-            :body-items="list1">
-        </form-preview>
+      <divider>项目类产品</divider>
+      <form-preview
+        class="project_part"
+        header-label="销量金额"
+        :header-value="'￥'+list1Total | numberComma"
+        :body-items="list1">
+      </form-preview>
 
-        <divider>A类产品</divider>
-        <form-preview 
-            class="project_part"
-            header-label="金额" 
-            :header-value="'￥'+list2Total | numberComma" 
-            :body-items="list2">
-        </form-preview>
-        <divider>B类产品</divider>
-        <form-preview 
-            class="project_part"
-            header-label="金额" 
-            :header-value="'￥'+list3Total | numberComma" 
-            :body-items="list3">
-        </form-preview>
+      <divider>A类产品</divider>
+      <form-preview
+        class="project_part"
+        header-label="销量金额"
+        :header-value="'￥'+list2Total | numberComma"
+        :body-items="list2">
+      </form-preview>
+      <divider>B类产品</divider>
+      <form-preview
+        class="project_part"
+        header-label="销量金额"
+        :header-value="'￥'+list3Total | numberComma"
+        :body-items="list3">
+      </form-preview>
 
     </div>
     <loading :show='spinner'></loading>
-   </div>
+  </div>
 </template>
 
 <script>
-import {
-  Tab,
-  TabItem,
-  Divider,
-  SwiperItem,
-  FormPreview,
-  numberComma,
-  CellFormPreview
-} from "vux";
-import myReportService from "service/myReportService";
-import { setTimeout } from "timers";
-import loading from "components/common/loading";
-const list = () => ["本日", "本周", "本月", "本年"];
-export default {
-  components: {
+  import {
     Tab,
     TabItem,
     Divider,
-    loading,
     SwiperItem,
     FormPreview,
+    numberComma,
     CellFormPreview
-  },
-  data() {
-    return {
-      listTab: list(),
-      demo2: "本日",
-      list1: [],
-      list2: [],
-      list3: [],
-      list1Total: 0,
-      list2Total: 0,
-      list3Total: 0,
-      remark: "",
-      spinner: false
-    };
-  },
-  filters: {
-    numberComma
-  },
-  methods: {
-    mylist(tab) {
-      if (tab == 0) {
-        this.listpanl(this.remark.days);
-      } else if (tab == 1) {
-        this.listpanl(this.remark.weeks);
-      } else if (tab == 2) {
-        this.listpanl(this.remark.months);
-      } else if (tab == 3) {
-        this.listpanl(this.remark.years);
-      }
-    },
-    listData() {
-      let ROSE_OPTION = JSON.parse(localStorage.getItem("ROSE_OPTION"));
-      let jsonData = {
-        shengName: ROSE_OPTION.region,
-        bankName: ROSE_OPTION.bank,
-        sybName: ROSE_OPTION.dept,
-        bmName: ROSE_OPTION.captain
-      };
-      myReportService
-        .myRepotList(jsonData)
-        .then(data => {
-          if (data) {
-            this.spinner = false;
-          }
-          this.remark = data;
-          this.mylist(0);
-        })
-        .catch(data => {
-          this.spinner = true;
-        });
-    },
-    listpanl(m) {
-      this.list1Total = 0;
-      this.list2Total = 0;
-      this.list3Total = 0;
-      this.list1.length = 0;
+  } from "vux";
+  import myReportService from "service/myReportService";
+  import {setTimeout} from "timers";
+  import loading from "components/common/loading";
+  // 方法引入
+  import {toFixed, accAdd, accMul} from 'plugins/calc'
 
-      for (let i = 0; i < m.length; i++) {
-        if (m[i].objType == "项目类产品" && m[i].objName) {
-          this.list1Total += Number(m[i].amount);
-          this.list1.push({
-            label: m[i].objName,
-            value: "￥" + numberComma(m[i].amount, 3) + " " + "(" + m[i].qty + "件/套" + ")"
-          });
-        } else if (m[i].objType == "A") {
-          this.list2Total = m[i].amount;
-        } else if (m[i].objType == "B") {
-          this.list3Total = m[i].amount;
+  const list = () => ["本日", "本周", "本月", "本年"];
+  export default {
+    components: {
+      Tab,
+      TabItem,
+      Divider,
+      loading,
+      SwiperItem,
+      FormPreview,
+      CellFormPreview
+    },
+    data() {
+      return {
+        listTab: list(),
+        demo2: "本日",
+        list1: [],
+        list2: [],
+        list3: [],
+        list1Total: 0,
+        list2Total: 0,
+        list3Total: 0,
+        remark: "",
+        spinner: false
+      };
+    },
+    filters: {
+      numberComma
+    },
+    methods: {
+      mylist(tab) {
+        if (tab == 0) {
+          this.listpanl(this.remark.days);
+        } else if (tab == 1) {
+          this.listpanl(this.remark.weeks);
+        } else if (tab == 2) {
+          this.listpanl(this.remark.months);
+        } else if (tab == 3) {
+          this.listpanl(this.remark.years);
         }
+      },
+      listData() {
+        let ROSE_OPTION = JSON.parse(localStorage.getItem("ROSE_OPTION"));
+        let jsonData = {
+          // shengName: ROSE_OPTION.region,
+          // bankName: ROSE_OPTION.bank,
+          // sybName: ROSE_OPTION.dept,
+          // bmName: ROSE_OPTION.captain,
+          shengName: '',
+          bankName: '',
+          sybName: '',
+          bmName: '',
+        };
+        myReportService
+          .myRepotList(jsonData)
+          .then(data => {
+            if (data) {
+              this.spinner = false;
+            }
+            this.remark = data;
+            this.mylist(0);
+          })
+          .catch(data => {
+            this.spinner = true;
+          });
+      },
+      listpanl(m) {
+        this.list1Total = 0;
+        this.list2Total = 0;
+        this.list3Total = 0;
+        this.list1.length = 0;
+        this.list2.length = 0;
+
+        for (let i = 0; i < m.length; i++) {
+          let item = m[i];
+          if (item.objType == "项目类产品" && item.objName) {
+            this.list1Total += Number(item.amount);
+            this.list1.push({
+              label: item.objName,
+              // value: "￥" + numberComma(item.amount, 3) + " " + "(" + item.qty + "件/套" + ")",
+              value: `￥${numberComma(item.amount, 3)} (${item.qty}件折合${item.coverNum || 0}套)`,
+            });
+          } else if (item.objType == "A") {
+            this.list2Total = item.amount;
+            this.list2 = [
+              {
+                label: '费用明细',
+                value: '',
+              }, {
+                label: '住宿费',
+                value: item.hotelCost || '无',
+              }, {
+                label: '其他费用',
+                value: item.elseCost || '无',
+              }, {
+                label: '合计',
+                value: item.hotelAndElseCost || '无',
+              }, {
+                label: '费用销售比',
+                // value: item.costSalesRatio || '无',
+                value: `${toFixed(accMul(item.hotelAndElseCost / item.amount, 100))}%` || '无',
+              },
+            ]
+          } else if (item.objType == "B") {
+            this.list3Total = item.amount;
+          }
+        }
+        this.list1Total = this.list1Total.toFixed(2);
       }
-      this.list1Total = this.list1Total.toFixed(2);
+    },
+    created() {
+      this.spinner = true;
+      this.listData();
     }
-  },
-  created() {
-    this.spinner = true;
-    this.listData();
-  }
-};
+  };
 </script>
 
 <style lang="less" scoped>
-@import "~vux/src/styles/1px.less";
-@import "~vux/src/styles/center.less";
-.tab-swiper {
-  background-color: #fff;
-  height: 100%;
-}
-.spinner_task {
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate3d(-50%, -50%, 0);
-  z-index: 99;
-}
+  @import "~vux/src/styles/1px.less";
+  @import "~vux/src/styles/center.less";
+
+  .tab-swiper {
+    background-color: #fff;
+    height: 100%;
+  }
+
+  .spinner_task {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate3d(-50%, -50%, 0);
+    z-index: 99;
+  }
 </style>
