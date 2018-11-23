0ecab8692f4b766393b093e9bb9e6514bd72c9aa
xuehongjie <jie.xue@refordom.com>
2018-11-23 13:52:11 +0800

    修改toFixed

--- src/plugins/calc/toFixed.js
+++ src/plugins/calc/toFixed.js
@@ -1,31 +1,24 @@
 // TODO 保留小数位
 export default (val, length = 2) => {
-  if (val === undefined) {
+  if (val === undefined || !/^\d*\.?\d*$/.test(val)) {
     console.error('the value can not change to number');
-    return
+    return ''
   }
 
-  let carry = 0; //存放进位标志
-  let num, multiple; //num为原浮点数放大multiple倍后的数，multiple为10的length次方
-  let str = val + ''; //将调用该方法的数字转为字符串
-  let dot = str.indexOf("."); //找到小数点的位置
-  // 找到要进行舍入的数的位置，手动判断是否大于等于5，满足条件进位标志置为1,这里原作者用的是str.substr(dot + length + 1, 1)
-  if (str.slice(dot + length + 1, dot + length + 2) >= 5) carry = 1;
-  multiple = Math.pow(10, length); //设置浮点数要扩大的倍数
-  num = Math.floor(val * multiple) + carry; //去掉舍入位后的所有数，然后加上我们的手动进位数
-  let result = num / multiple + ''; //将进位后的整数再缩小为原浮点数
-  // 处理进位后无小数
-  dot = result.indexOf(".");
-  if (dot === -1) {
-    result += '.';
-    dot = result.indexOf(".");
+  let [num = '', dec = ''] = `${val}`.split('.'); // 获取整数值和小数值
+  // dec = dec.padEnd(length, '0'); // 将0 补全
+  if (dec.length < length) {
+    dec = `${dec}${'0'.repeat(length - dec.length)}`; // 将0 补全
   }
-  // 处理多次进位
-  let len = result.length - (dot + 1);
-  if (len < length) {
-    for (let i = 0; i < length - len; i++) {
-      result += 0;
+  // 小数位长度多于要保留的长度
+  if (dec.length !== length) {
+    dec = `${dec.substr(0, length)}.${dec.substr(length)}`; // 给要保留的位数后加小数点
+    // dec = `${Math.round(dec)}`.padStart(length, '0'); // 往前面补0，处理类似001这种情况
+    dec = `${Math.round(dec)}`; // 四舍五入
+    if (dec.length < length) {
+      dec = `${'0'.repeat(length - dec.length)}${dec}`; // 往前面补0，处理类似001这种情况
     }
   }
-  return parseFloat(result);
+
+  return parseFloat(`${num}.${dec}`)
 }
