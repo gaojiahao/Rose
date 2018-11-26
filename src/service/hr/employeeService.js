f8794d6caadc4a8d159714803d8042e8514e66c3
baixia123 <xia.bai@refordom.com>
2018-11-26 19:22:21 +0800

    新增接口

---
+++ src/service/hr/employeeService.js
@@ -0,0 +1,31 @@
+
+import $axios from 'plugins/ajax';
+
+// 获取 所有部门列表
+export let getAllGroup = () => {
+  return $axios.ajax({
+    url: '/H_roleplay-si/ds/getAllGroup',
+    data: {
+      page: 1,
+      start: 0,
+      limit: 1000
+    }
+  })
+}
+// 获取所有岗位列表
+export let getAllRoleInfo = () => {
+  return $axios.ajax({
+    url: '/H_roleplay-si/ds/getAllRoleInfo',
+    data: {
+      page: 1,
+      start: 0,
+      limit: 1000
+    }
+  })
+}
+
+export default {
+  getAllGroup,
+  getAllRoleInfo
+
+}
