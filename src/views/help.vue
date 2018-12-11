fb618156752d238e0ff7bdbbecce741dae435571
刘治增 <zionstark.liu@refordom.com>
2018-12-11 11:46:26 +0800

    完善套合计

--- src/views/help.vue
+++ src/views/help.vue
@@ -336,7 +336,7 @@
         // 项目类产品
         for (let item of this.arr) {
           // 动态增加 月销量“套”合计
-          newVar2 = toFixed(newVar2, Number(item.num1));
+          newVar2 = toFixed(accAdd(newVar2, Number(item.num1)));
           // 项目类产品
           jsonData.transDetailUncalc.push({
             id: this.guid(),
