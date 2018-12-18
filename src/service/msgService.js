295a44a3504eecdbd3a253b892a9f8602c433bc5
刘治增 <zionstark.liu@refordom.com>
2018-12-18 16:53:41 +0800

    修改消息列表映射表

--- src/service/msgService.js
+++ src/service/msgService.js
@@ -4,7 +4,7 @@ import $flyio from '../plugins/ajax';
 // TODO 消息代办列表
 export let getMsgList = (data = {}) => {
     return $flyio.ajax({
-        url:'/H_roleplay-si/ds/getTasksListData?entityId=20000&para1='
+        url:'/H_roleplay-si/ds/getTasksListData'
     })
 }
 // TODO 所有消息列表
