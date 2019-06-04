5a0694e1dfc4181af7d28081f000b9bfde19943f
Xing <xing.jiang@refordom.com>
2019-06-04 18:49:43 +0800

    修改概览是的样式，改成一块一块的

--- src/components/public/Fieldset.vue
+++ src/components/public/Fieldset.vue
@@ -11,8 +11,10 @@
       </header>
       <div class="readOnlyPart" v-if="readOnlyParts.length && styleType == 0">
         <template v-for="(item, index) in readOnlyParts">
-          <span :key="index">{{item.fieldLabel}}：</span>
-          <span>{{values[item.fieldCode]||'无'}}</span>
+          <div class="item">
+            <span :key="index">{{item.fieldLabel}}：</span>
+            <span>{{values[item.fieldCode]||'无'}}</span>
+          </div>
         </template>
       </div>
       <template v-for="(item, index) in editParts">
@@ -69,7 +71,9 @@ var component = {
           // 当Grid组件只读为false时 各个字段的readOnly才能启用
           if (
             item.readOnly == true &&
-            item.hiddenInRun == false &&
+            (item.hiddenInRun === undefined ||
+              item.hiddenInRun === null ||
+              item.hiddenInRun === false) &&
             cfg.layout != "fit"
           ) {
             readOnlyParts.push(item);
@@ -133,10 +137,15 @@ export default Vue.component("RFieldset", component);
     line-height: 0.22rem;
     font-size: 0.12rem;
     span:nth-child(2n + 1) {
-      color: #999;
+      color: #aaa;
     }
     span:nth-child(2n) {
-      margin-right: 0.05rem;
+      font-weight: 400;
+      font-size: 0.13rem;
+    }
+    .item {
+      display: inline-flex;
+      margin-right: 0.2rem;
     }
   }
   .each_property {
--- src/components/public/Grid.vue
+++ src/components/public/Grid.vue
@@ -1,67 +1,88 @@
 <template>
   <div class="r-grid">
     <div class="r-row vux-1px-b" v-for="(row,rIndex) in value">
-      <template v-for="(item, index) in cfg.columns" class="cell  when-is-right" v-if="item.hidden == false">
-         <span :key="index">{{item.text}}：</span><span>{{row[item.fieldCode]||'无'}}</span>
+      <template
+        v-for="(item, index) in cfg.columns"
+        class="cell when-is-right"
+        v-if="item.hidden == false"
+      >
+        <div class="item">
+          <span :key="index">{{item.text}}：</span>
+          <span>{{row[item.fieldCode]||'无'}}</span>
+        </div>
       </template>
-      <div @click="onShowMore(row,rIndex)" class="show-more">查看详情<i class="icon-more"></i></div>
+      <div @click="onShowMore(row,rIndex)" class="show-more">
+        查看详情
+        <i class="icon-more"></i>
+      </div>
     </div>
-    <grid-detail :show="showDetail" :item ="detail" :columns="cfg.columns" v-model="showDetail" @on-confirm="doDetailEdit"/>
+    <grid-detail
+      :show="showDetail"
+      :item="detail"
+      :columns="cfg.columns"
+      v-model="showDetail"
+      @on-confirm="doDetailEdit"
+    />
   </div>
 </template>
 <script>
-import Vue from 'vue';
+import Vue from "vue";
 var component = {
-    props:['cfg','value'],
-    data(){
-      return {
-        showDetail:false,
-        detail:{}
-      }
-    },
-    methods:{
-      doDetailEdit(){},
-      checkAmt(){},
-      onShowMore(row){
-         this.detail = row;
-         this.showDetail = true;
-      }
+  props: ["cfg", "value"],
+  data() {
+    return {
+      showDetail: false,
+      detail: {}
+    };
+  },
+  methods: {
+    doDetailEdit() {},
+    checkAmt() {},
+    onShowMore(row) {
+      this.detail = row;
+      this.showDetail = true;
     }
-}
-export default Vue.component('RGrid',component)
+  }
+};
+export default Vue.component("RGrid", component);
 </script>
 
 <style lang="scss">
-.r-row{
+.r-row {
   margin: 0.05rem 0 0.05rem;
-  line-height: .22rem;
-  font-size: .12rem;
-  span:nth-child(2n+1){
-    color:#999;
+  line-height: 0.22rem;
+  font-size: 0.12rem;
+  span:nth-child(2n + 1) {
+    color: #aaa;
   }
-  span:nth-child(2n){
-    margin-right:0.05rem;
+  span:nth-child(2n) {
+    font-weight: 400;
+    font-size: 0.13rem;
   }
   &.vux-1px-b:after {
     border-color: #e8e8e8;
   }
-   &.vux-1px-b:last-child:after{
-     border:none;
-   }
-  .show-more{
-    text-align:right;
-    color:blue;
+  &.vux-1px-b:last-child:after {
+    border: none;
   }
-  .icon-more{
-      display: inline-block;
-      width: .2rem;
-      height: .04rem;
+  .show-more {
+    text-align: right;
+    color: blue;
+  }
+  .icon-more {
+    display: inline-block;
+    width: 0.2rem;
+    height: 0.04rem;
+  }
+  .item {
+    display: inline-flex;
+    margin-right: 0.2rem;
   }
 }
-.r-row:first-child{
-  margin-top:0rem;
+.r-row:first-child {
+  margin-top: 0rem;
 }
-.r-row:last-child{
-  margin-bottom:0rem;
+.r-row:last-child {
+  margin-bottom: 0rem;
 }
 </style>
