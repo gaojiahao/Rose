ea2a014045409f5cac56e7514da6997f7c85f8c1
gjh <hao.gao@refordom.com>
2019-06-05 11:04:32 +0800

    fixbug(重复项渲染时可能显示不出数据的问题)

--- src/components/public/Fieldset.vue
+++ src/components/public/Fieldset.vue
@@ -13,7 +13,8 @@
         <template v-for="(item, index) in readOnlyParts">
           <div class="item">
             <span :key="index">{{item.fieldLabel}}：</span>
-            <span>{{values[item.fieldCode]||'无'}}</span>
+            <span v-if="!item.text">{{values[item.fieldCode]||'无'}}</span>
+            <span v-else>{{data[item.fieldCode]||'无'}}</span>
           </div>
         </template>
       </div>
@@ -55,15 +56,24 @@ var component = {
       visibleItemsLength: 0, //
       hasToogleBar: false,
       editParts: [],
-      readOnlyParts: []
+      readOnlyParts: [],
+      data:[],
     };
   },
   created: function() {},
   watch: {
+    values: {
+      handler() {
+        let name = this.cfg.name;
+        let data = this.values;
+        this.data = data && data[name];
+        this.data = (this.data && this.data[0]) || [];
+      }
+    },
     cfg: {
       handler(cfg) {
         // *部分应用* 物料详情在审批节点可以重新录入数据 此处进行数据分割
-        let { items = [] } = cfg,
+        let { items = [],columns = [] } = cfg,
           formModel = this.form.model;
         let readOnlyParts = [],
           i = 0;
@@ -76,6 +86,20 @@ var component = {
               item.hiddenInRun === false) &&
             cfg.layout != "fit"
           ) {
+            readOnlyParts.push(item);
+          }
+          if (!item.hiddenInRun) i++;
+        });
+        //重复项的配置
+        columns.forEach(item => {
+          // 当Grid组件只读为false时 各个字段的readOnly才能启用
+          if (
+            item.readOnly == true &&
+            (item.hidden === undefined ||
+              item.hidden === null ||
+              item.hidden === false)
+          ) {
+            item.fieldLabel = item.text;
             readOnlyParts.push(item);
           }
           if (!item.hiddenInRun) i++;
