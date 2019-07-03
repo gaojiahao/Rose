c384befaaaf6bc3016dd8b00508270bc0cc1ec44
Xing <xing.jiang@refordom.com>
2019-07-03 11:19:50 +0800

    美化代码

--- src/mixins/grid.js
+++ src/mixins/grid.js
@@ -8,59 +8,59 @@ import {
 export default {
     data() {
         return {
-            isEdit:false,
-            selection:[]
+            isEdit: false,
+            selection: []
         };
     },
-    methods:{
-        addRecords:function(selection){
-            var value = this.getValue()||[],
-                record, 
-                row,i=0,l = selection.length;
-        
-            for(i;i<l;i++){
-              row = selection[i];
-              record = this.createRecord(row);
-              value.push(record.data);
+    methods: {
+        addRecords: function (selection) {
+            var value = this.getValue() || [],
+                record,
+                row, i = 0, l = selection.length;
+
+            for (i; i < l; i++) {
+                row = selection[i];
+                record = this.createRecord(row);
+                value.push(record.data);
             }
             this.setValue(value);
         },
-        createRecord:function(row){
-             var me = this,
-                 data = {},
-                 record = new Record(data,me),
-                 editorFieldCode,
-                 dataSourceBind = this.dataSourceBind;
-                
-             this.setDefaultValue(record);
-             if(dataSourceBind){
-               editorFieldCode = dataSourceBind.k;
-               data[editorFieldCode] = row[dataSourceBind.v];
-               this.setValueBindValue(record,editorFieldCode,row);
-             }
-             me.executeExpression(record,editorFieldCode);
-             return record;
+        createRecord: function (row) {
+            var me = this,
+                data = {},
+                record = new Record(data, me),
+                editorFieldCode,
+                dataSourceBind = this.dataSourceBind;
+
+            this.setDefaultValue(record);
+            if (dataSourceBind) {
+                editorFieldCode = dataSourceBind.k;
+                data[editorFieldCode] = row[dataSourceBind.v];
+                this.setValueBindValue(record, editorFieldCode, row);
+            }
+            me.executeExpression(record, editorFieldCode);
+            return record;
         },
-        checkAll(){
-           // 如果已全部选中 则清除所有选中状态
+        checkAll() {
+            // 如果已全部选中 则清除所有选中状态
             if (this.selection.length === this.values.length) {
                 this.selection = [];
-            }else{
+            } else {
                 this.selection = this.values.map((item, index) => index);
             }
-           
+
         },
-        doDelete:function(){
+        doDelete: function () {
             this.$vux.confirm.show({
                 content: '确认删除?',
                 // 确定回调
-                onConfirm:()=>{
+                onConfirm: () => {
                     var selection = this.selection,
                         rowIndex,
                         newValues = [];
 
-                    this.values.forEach((row,rowIndex)=>{
-                        if(selection.indexOf(rowIndex) == -1){
+                    this.values.forEach((row, rowIndex) => {
+                        if (selection.indexOf(rowIndex) == -1) {
                             newValues.push(row);
                         }
                     })
@@ -71,24 +71,24 @@ export default {
         },
         doDetailEdit(data) {
             var value = util.clone(data);
-            this.$set(this.values,this.detailRowNumer,data);
+            this.$set(this.values, this.detailRowNumer, data);
         },
-        delClick(rowIndex){
-           var selection = this.selection, 
-               index;
-           
-           if(selection.length){
-               index = selection.indexOf(rowIndex);
-               if(index != -1){
-                   selection.splice(index,1);
-               }else{
-                   selection.push(rowIndex);
-               }
-           } else {
-               selection.push(rowIndex);
-           }
+        delClick(rowIndex) {
+            var selection = this.selection,
+                index;
+
+            if (selection.length) {
+                index = selection.indexOf(rowIndex);
+                if (index != -1) {
+                    selection.splice(index, 1);
+                } else {
+                    selection.push(rowIndex);
+                }
+            } else {
+                selection.push(rowIndex);
+            }
         },
-        executeExpression(record,editorFieldCode){
+        executeExpression(record, editorFieldCode) {
             var me = this,
                 expressionCfg = me.expressionCfg,
                 i, l,
@@ -103,8 +103,8 @@ export default {
                 loanDay = function (qty, date) {
                     return util.dateAdd((new Date(date)), util.DAY, 0 - Number(qty));
                 };
-    
-            
+
+
             if (expressionCfg) for (i = 0, l = expressionCfg.length; i < l; i++) {
                 cfg = expressionCfg[i];
                 dataIndex = cfg.col.fieldCode;
@@ -131,7 +131,7 @@ export default {
                 var changeItems = record.getChanges(),
                     v1,
                     v2;
-    
+
                 if (cfg.v1 in changeItems || cfg.v2 in changeItems) {
                     v1 = record.get(cfg.v1);
                     v2 = record.get(cfg.v2);
@@ -149,25 +149,25 @@ export default {
                 return cfg;
             }
         },
-        getSubmitData:function(){
+        getSubmitData: function () {
             var values = this.getValue(),
-                data = {dataSet:[]},
+                data = { dataSet: [] },
                 columns = this.cfg.columns,
-                submitFieldCodes =  columns.filter(function (c) {
+                submitFieldCodes = columns.filter(function (c) {
                     return c.submitValue;
-                }).map(function(c){ return c.fieldCode }),
+                }).map(function (c) { return c.fieldCode }),
                 newValue = data.dataSet,
                 key,
                 value,
                 newRow;
 
-            values.map(function(row){
+            values.map(function (row) {
                 newRow = {};
-                util.each(submitFieldCodes,function(key){
+                util.each(submitFieldCodes, function (key) {
                     value = row[key];
-                    if(typeof value == 'undefined'){
+                    if (typeof value == 'undefined') {
                         newRow[key] = null;
-                    }else if(typeof value != 'object'){
+                    } else if (typeof value != 'object') {
                         newRow[key] = row[key];
                     }
                 });
@@ -176,21 +176,21 @@ export default {
 
             return data;
         },
-        getComponentByCfg:function(cfg){
-           if(cfg.contrl){
-               return this.form.fieldMap[cfg.contrl];
-           }
+        getComponentByCfg: function (cfg) {
+            if (cfg.contrl) {
+                return this.form.fieldMap[cfg.contrl];
+            }
         },
-        getValueByConfig(cfg){
+        getValueByConfig(cfg) {
             var value,
                 cmp;
 
-            if(cfg.type == 'formData'){
+            if (cfg.type == 'formData') {
                 cmp = this.getComponentByCfg(cfg.data);
-                if(cmp) value = cmp.getExtraFieldValue(cfg.data.valueField);
-            }else if(cfg.type == 'contextData'){
+                if (cmp) value = cmp.getExtraFieldValue(cfg.data.valueField);
+            } else if (cfg.type == 'contextData') {
                 value = getValuesByExp(cfg.data);
-            } else if(cfg.type == 'staticData'){
+            } else if (cfg.type == 'staticData') {
                 value = cfg.data[0];
             }
             return value;
@@ -222,13 +222,13 @@ export default {
                                 if (valuebind && editorDataIndex in valuebind) {
                                     cfgArr = valuebind[editorDataIndex];
                                     Ext.each(cfgArr, function (cfg) {
-                                        if(record.get(cfg.dataIndex) != null){
-                                            record.set(cfg.dataIndex,undefined);
+                                        if (record.get(cfg.dataIndex) != null) {
+                                            record.set(cfg.dataIndex, undefined);
                                             loopfn(cfg.dataIndex);
                                         }
-                                        
+
                                     })
-                                }    
+                                }
                                 loopfn(editorDataIndex);
                             }
                         }
@@ -241,7 +241,7 @@ export default {
                 loopfn(changeDataIndex);
             }
         },
-        handlerValueBind:function(record,selection,editorFieldCode,cfgArr){
+        handlerValueBind: function (record, selection, editorFieldCode, cfgArr) {
             var extra = {};
 
             extra[editorFieldCode + '.extraData'] = util.clone(selection);
@@ -252,7 +252,7 @@ export default {
 
                 try {
                     value = util.isArray(valueField) ? util.getValueByNs(extra, valueField) : eval('extra' + valueField);
-                    record.set(targetFieldCode,convertDataType(cfg.editorType, value));
+                    record.set(targetFieldCode, convertDataType(cfg.editorType, value));
                 } catch (ex) {
                     console.warn(ex);
                 }
@@ -266,30 +266,30 @@ export default {
             if (bindCmp.changeEventSource == 'setVal') return;
             if (column.selectionModel == 'SINGLE') {
                 //dataIndex
-                if(values)values.forEach(function (rec) {
-                    me.$set(rec,dataIndex, undefined);
+                if (values) values.forEach(function (rec) {
+                    me.$set(rec, dataIndex, undefined);
                 });
             } else {
                 me.setValue([]);
             }
         },
-        isChecked(rowIndex){
+        isChecked(rowIndex) {
             return this.selection.indexOf(rowIndex) != -1;
         },
-        isCheckAll(){
-           return this.values && this.values.length == this.selection.length;
+        isCheckAll() {
+            return this.values && this.values.length == this.selection.length;
         },
-        isValid:function(){
+        isValid: function () {
             var me = this,
                 value = me.getValue();
 
-            if(value == null || value.length == 0){
+            if (value == null || value.length == 0) {
                 me.$vux.alert.show({
-                    content: '请选择' + (me.listTitle||'物料')
+                    content: '请选择' + (me.listTitle || '物料')
                 });
                 return false;
             }
-            return me.validateData(value,me.cfg.columns);
+            return me.validateData(value, me.cfg.columns);
         },
         initDefaultValueCfg: function () {
             var me = this,
@@ -316,11 +316,11 @@ export default {
                         cmpId = cmp.cfg.id;
 
                         if (cmp && cmpId != gridId) {
-                            form.$on('value-change-' + cmpId,function (cmp, e) {
+                            form.$on('value-change-' + cmpId, function (cmp, e) {
                                 var value = '';
                                 if (cmp == null || cmp.changeEventSource == 'setVal') return;
                                 value = cmp.getExtraFieldValue(cfg.data.valueField);
-                                me.resetDefaultValue(c.fieldCode,value);
+                                me.resetDefaultValue(c.fieldCode, value);
                             });
                             cfgArr.push(c);
                         }
@@ -352,7 +352,7 @@ export default {
                     dsDataIndex,
                     dsArr;
 
-                if ( !util.isEmpty(ds)) {
+                if (!util.isEmpty(ds)) {
                     dsArr = util.isArray(ds) ? ds : [ds];
                     dsArr.forEach(function (ds) {
                         if (ds.type === 'formData' && ds.data.contrl === me.cfg.id) {
@@ -512,42 +512,42 @@ export default {
                 me.paramColumns = paramColumns;
             }
         },
-        resetDefaultValue(fieldCode,value){
+        resetDefaultValue(fieldCode, value) {
             var me = this,
                 values = me.getValue();
 
-            if(values)values.forEach(function (row) {
-                me.$set(row,fieldCode, value);
+            if (values) values.forEach(function (row) {
+                me.$set(row, fieldCode, value);
                 //me.handleParamChange(row);//处理数据变化
             });
         },
-        setDefaultValue(record){
+        setDefaultValue(record) {
             var me = this,
                 cols = me.defaultValueCfgArr,
                 cfg,
                 value;
-               
+
             cols.forEach(function (c) {
-                 cfg = util.isString(c.defaultValue) ? JSON.parse(c.defaultValue) : c.defaultValue;
-                 value = me.getValueByConfig(cfg);
-                 record.set(c.fieldCode,convertDataType(c.editorType, value));
+                cfg = util.isString(c.defaultValue) ? JSON.parse(c.defaultValue) : c.defaultValue;
+                value = me.getValueByConfig(cfg);
+                record.set(c.fieldCode, convertDataType(c.editorType, value));
             });
         },
-        setValueBindValue(record,editorFieldCode,selection){
+        setValueBindValue(record, editorFieldCode, selection) {
             var me = this,
                 valuebind = me.valueBindCfg,
                 cfgArr;
 
             if (valuebind && editorFieldCode in valuebind) {
                 cfgArr = valuebind[editorFieldCode];
-                me.handlerValueBind(record,selection,editorFieldCode,cfgArr);
-            }   
+                me.handlerValueBind(record, selection, editorFieldCode, cfgArr);
+            }
         },
-        toggleEditStatus(){
+        toggleEditStatus() {
             this.isEdit = !this.isEdit;
             this.selection = [];
         },
-        validateData:function(value,columns){
+        validateData: function (value, columns) {
             var me = this,
                 columnHash = {},
                 disallowBlankCols = [],
@@ -583,7 +583,7 @@ export default {
                         return Date((record.data[col.fieldCode]).replace(/-/g, '/')).getTime();
                     }
                 };
-    
+
             (function (columns) {
                 var i,
                     l = columns.length,
@@ -607,11 +607,11 @@ export default {
                     }
                 }
             })(columns);
-    
-            util.each(value,function (row, index) {
-                var record = new Record(row,me);
 
-                util.each(disallowBlankCols,function (col) {
+            util.each(value, function (row, index) {
+                var record = new Record(row, me);
+
+                util.each(disallowBlankCols, function (col) {
                     if (util.isEmpty(row[col.fieldCode])) {
                         errorMsgArr.push('重复项第【' + (index + 1) + '】项的【' + col.text + '】未填写!');
                         validResult = false;
@@ -633,7 +633,7 @@ export default {
 
                 if (validResult) {
                     util.each(numberVerifyCols, function (col) {
-                        var i,l,conditions,
+                        var i, l, conditions,
                             key,
                             numberVerify = col.numberVerify;
 
@@ -654,7 +654,7 @@ export default {
                                     return false;
                                 }
                             }
-                           
+
                         } else {
                             errorMsgArr.push('【' + nowColmn + '】未配置校验关联列');
                             validResult = false;
@@ -665,7 +665,7 @@ export default {
                     return false;
                 }
             });
-            if (errorMsgArr.length)me.$vux.alert.show({
+            if (errorMsgArr.length) me.$vux.alert.show({
                 content: errorMsgArr.join('</br>')
             });
             return validResult;
