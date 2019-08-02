1e64c42faa51cc778277c1679d499a06ade3c82d
shi <hua.shi@roletask.com>
2019-08-02 17:17:27 +0800

    fixedBug:修复texteare样式。

--- src/components/public/R2TextArea.vue
+++ src/components/public/R2TextArea.vue
@@ -1,17 +1,17 @@
 <template>
-<div v-show="!hidden" class="vux-1px-b textarea">
-    <group>
-        <x-textarea v-if="cfg.readOnly == false" :value="values[cfg.fieldCode]" placeholder="请输入" :max="100" class="fontSize" @input="onInput"></x-textarea>
-    </group>
-</div>
+    <x-textarea :value="values[cfg.fieldCode]" placeholder="请输入" :max="100" class="each_property textarea" @input="onInput" :readonly = "cfg.readOnly">
+        <template slot="label">
+        <label :class="{required : !cfg.allowBlank,'readonly':cfg.readOnly}" style="display: block; width: 3em;">{{cfg.fieldLabel}}</label>
+        </template>
+    </x-textarea>
 </template>
 <script>
 import Vue from 'vue'
-import { Group,XTextarea } from 'vux'
+import {XTextarea } from 'vux'
 import fieldBase from 'mixins/fieldBase'
 let  cfg = {
      mixins:[fieldBase],
-    components: { Group,XTextarea },
+    components: {XTextarea },
      props:['cfg','values'], 
      methods:{
         onInput:function(e){
@@ -24,24 +24,12 @@ export default Vue.component('R2TextArea',cfg);
 </script>
 <style lang="scss">
 @import '~@/scss/color';
-.textarea {
-    .weui-cell {
-        padding: 0;
-    }
-    .weui-textarea {
-        color: #333;
-    }
-    .content {
-        display: flex;
-        align-items: center;
-        .icon-right{
-            width: .08rem;
-            height: .14rem;
-            margin-left: .1rem;
-        }
-    }
-    .fontSize {
-      font-size: .14rem;
+.r-fieldset .textarea {
+    height:auto;
+    line-height: normal;
+    align-items:normal;
+    &:before{
+        border-top:none;
     }
 }
 </style>
