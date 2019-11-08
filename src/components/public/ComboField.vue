c10545630f3d11d403e2042390f13b7e81189916
gaojiahao <hao.gao@refordom.com>
2019-11-08 11:26:31 +0800

    fixbug(错误提示bug)

--- src/components/public/ComboField.vue
+++ src/components/public/ComboField.vue
@@ -413,7 +413,7 @@ let cfg = {
       this.selection = null;
       this.value = null;
       this.setValue(null);
-      if(this.form.$parent.form.model == 'new'){
+      if(!this.cfg.defaultValue){
         this.displaysValue = '';
       }
     },
