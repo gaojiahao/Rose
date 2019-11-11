d7a1216f70c347597dc63d05c913807e8ad8c1cd
snack <snack.huang@refordom.com>
2019-11-11 13:41:58 +0800

    fixbug:托盘码正则校验先去除

--- src/components/apply/stock/sortoutApply.vue
+++ src/components/apply/stock/sortoutApply.vue
@@ -397,7 +397,7 @@ export default {
             }
         },
         handlerScanBoxCode(){
-             //matCode 物料编码
+            //matCode 物料编码
             //batchNo 批次号
             //productionDate 生产日期
             //boxRule 箱规
