e4ecb3e91a6a12c6eb6068b55626dd79a9041aea
gaojiahao <hao.gao@refordom.com>
2019-12-31 17:47:11 +0800

    钉钉测试

--- src/service/tokenService.js
+++ src/service/tokenService.js
@@ -201,7 +201,7 @@ let tokenService = {
                 key1: data.key1,
                 active: data.active
               });
-              resolve(data[key])
+              resolve(data['token'])
             }).catch(function (error) {
               let res = error.response;
               let data = (res && res.data) || {};
