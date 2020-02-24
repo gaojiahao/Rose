011e52b75b0cc15b78c6f9b4813331667e6abf0e
gaojiahao <1026844853@qq.com>
2020-02-24 17:12:15 +0800

    fixbug(combo控件bug)

--- src/components/public/ComboField.vue
+++ src/components/public/ComboField.vue
@@ -126,6 +126,7 @@ let cfg = {
                 if(res){
                   this.displaysValue = res[this.cfg.displayField];
                   this.selection = res;
+                  this.form.$emit(this.valueChangeKey,this);
                 }else{
                   this.displaysValue = this.values[this.cfg.fieldCode];
                 }
@@ -470,10 +471,10 @@ let cfg = {
             this.selItem(selection);
             return ;
           }
-          if(selection == null&&!this.cfg.valueBind) {
-            this.reSet();
-            this.displaysValue = '';
-          }
+          // if(selection == null&&!this.cfg.valueBind) {
+          //   this.reSet();
+          //   this.displaysValue = '';
+          // }
       } else {
         if(listData.length&&this.cfg.defaultValue){
           selection = listData[0];
