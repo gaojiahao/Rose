ea50809e25670563b790f08adebbdf19467a0289
shi <hua.shi@roletask.com>
2020-04-26 11:56:00 +0800

    fixdbug:修复登陆后聊天页面处于loading的bug.

--- src/views/msg/navigation.vue
+++ src/views/msg/navigation.vue
@@ -76,6 +76,10 @@ export default {
         })
     },
     activated:function(){
+        if(this.refresh){//如果需要刷新
+            this.refresh = false;
+            this.initGroup();
+        }
     },
     data(){
         return {
@@ -126,6 +130,10 @@ export default {
                 this.groups = data;
                 this.scroller && this.scroller.refresh();
                 this.showLoading = false
+            }).catch(e=>{
+                if(e.message == 'nologin'){//没有登录
+                    this.refresh = true;
+                }
             })
         },
         initDs:function(){
