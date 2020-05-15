caf4b6cde302b5610b955cd4fc3910b3fb0cb792
wangxiaoying <cherry.wang@refordom.com>
2020-05-15 14:02:17 +0800

    修改成员选择器样式问题

--- src/views/msg/navigation.vue
+++ src/views/msg/navigation.vue
@@ -305,6 +305,7 @@ export default {
         },
         showCreateGroupList() {
             var memberSelector = this.$refs["memberSelector"];
+            this.showList = false;
             memberSelector.showMemberSelector = true;
         },
         addGroup(userList) {
