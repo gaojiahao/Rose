9b669f26843fb1055a64fa23808ae32adbd8c02c
gaojiahao <1026844853@qq.com>
2020-07-24 14:49:43 +0800

    更新新的微信配置

--- src/plugins/ajax/conf.js
+++ src/plugins/ajax/conf.js
@@ -4,7 +4,7 @@
  */
 
 export const corpid = 'wx5311bd8608c14d98'; // 企业id
-export const corpsecret = 'JjpZWLy0D4QVxCHwMqQRDMFt93qzm9nroaZKjgBQTzc'; // 应用凭证
+export const corpsecret = 'JjpZWLy0D4QVxCHwMqQRDJch5NxJoc_JRcJVwExzXjQ'; // 应用凭证
 export const agentid = '1000011'; // 企业应用的id
 export const redirect_uri = 'https%3a%2f%2fwww.gofuit.com%2fZeus'; // 首页路径
 
